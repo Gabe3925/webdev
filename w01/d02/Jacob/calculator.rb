1804252a97f595633419a360c16162509c89dbc5
jcain3389 <jcain3389@yahoo.com>
2014-04-22 18:14:21 -0400

    commit 2

---
+++ w01/d02/Jacob/calculator.rb
@@ -0,0 +1,23 @@
+def add(addend_1, addend_2)
+  return addend_1 + addend_2
+end
+
+def subtract(minuend, subtrahend)
+  return minuend - subtrahend
+end
+
+def multiply(multiplicand, multiplier)
+  return multiplicand*multiplier
+end
+
+def divide(dividend, divisor)
+  return dividend/divisor
+end
+
+def exponent(base, exponent)
+  return base**exponent
+end
+
+def square_root(number)
+  return number**0.5
+end
