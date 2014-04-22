79e77dddcdb32b8d2dcb7e4371a27e8330f72ee1
joellastraley <jstraley@gmail.com>
2014-04-22 17:32:34 -0400

    initial commit, creates basic calculator functionality

---
+++ w01/d02/JoElla/calculator.rb
@@ -0,0 +1,22 @@
+
+#make a function for each math operation: add, subtract, multiply, divide
+
+
+
+def add(num1, num2)
+  return puts num1 + num2
+end
+
+def subtract(num1, num2)
+  return puts num1 - num2
+end
+
+def multiply(num1, num2)
+  return puts num1 * num2
+end
+
+def divide(num1, num2)
+  return puts num1 * num2
+end
+
+
