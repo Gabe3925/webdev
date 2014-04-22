0527f2fe4e6d76878e54944dfb85c96bd4205a82
jcain3389 <jcain3389@yahoo.com>
2014-04-22 16:12:30 -0400

    area, volume, lyric

---
+++ w01/d02/Jacob/area_method.rb
@@ -0,0 +1,11 @@
+def calc_area(length, width)
+  return length * width
+end
+
+def calc_vol(length, width, height)
+  return length * width * height
+end
+
+def sings_fav_lyric(fav_lyric)
+  return fav_lyric.upcase + "!!!"
+end
