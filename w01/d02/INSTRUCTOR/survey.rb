1524be42ecc399080f24468958c5ff37e422fdcc
vanderhoop <vanderhoop@me.com>
2014-04-22 13:58:49 -0400

    changes to obi-wan

---
+++ w01/d02/INSTRUCTOR/survey.rb
@@ -0,0 +1,13 @@
+puts "What's your name?"
+name = gets.chomp
+
+puts "What do you look for when choosing an insurance company?"
+insurance_prefs = gets.chomp
+
+puts "Who is your current car insurance provider?"
+insurance_provider = gets.chomp
+
+puts "Have you ever had a car accident?"
+answer = gets.chomp
+
+
