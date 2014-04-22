eab1b3a7892db16c4a4e0fd7a41d1e38f5fb5f81
stephenstanwood <stephen@stanwoodsolutions.com>
2014-04-22 10:08:37 -0400

    1.2 fixed else branch

--- w01/d02/Stephen/searching_for_obi_wan.rb
+++ w01/d02/Stephen/searching_for_obi_wan.rb
@@ -9,8 +9,9 @@ if response.downcase == "i do"
   puts "Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you."
 else
   puts  "I've really enjoyed speaking with you, #{name}, but if you'll please excuse me, I have to help my friend find someone named Obi-Wan Kenobi."
+  puts "What's your favorite farewell?"
+  puts "#{gets.chomp} to you too."
+  puts "Well R2, I suppose we'll just have to keep looking."
+  puts "R2-D2: (Agreeable droid noises)"
 end
-puts "What's your favorite farewell?"
-puts "#{gets.chomp} to you too."
-puts "Well R2, I suppose we'll just have to keep looking."
-puts "R2-D2: (Agreeable droid noises)"
+
