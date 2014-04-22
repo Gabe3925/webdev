14b05a8af98d1647fd7a43e0a504b2877d8af75d
BYWallace <bwallace90@gmail.com>
2014-04-22 19:52:54 -0400

    added basic arithmetic functions to calculator (+, -, x, /)

---
+++ w01/d02/Brett/calculator.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+def add(x, y)
+  return x + y
+end
+
+def subtract(x, y)
+  return x - y
+end
+
+def multiply(x, y)
+  return x * y
+end
+
+def divide(x, y)
+  return x / y
+end
+
+binding.pry
