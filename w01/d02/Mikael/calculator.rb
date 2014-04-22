1f4fb999d10e24ee3f3178f6294d64567b89911e
OKitsMikael <mgd4@njit.edu>
2014-04-22 18:01:12 -0400

    Day 2 homework commit 2

--- w01/d02/Mikael/calculator.rb
+++ w01/d02/Mikael/calculator.rb
@@ -21,7 +21,7 @@ end
 
 
 puts "Welcome to the calculator!"
-puts "You may add, subtract, multiply, divide, or exponentiate."
+puts "You may add, subtract, multiply, divide, exponentiate, or square root."
 puts "Please enter an operation, followed by the first operator, followed by the second operator."
 calculate("add",3,5)
 calculate("exponentiate",2,3)
