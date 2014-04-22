846b3eafe1bc6c2b7b14054a4167b880d2ac5cfc
jcain3389 <jcain3389@yahoo.com>
2014-04-22 17:25:26 -0400

    methods modifications

--- w01/d02/Jacob/methods_exercise.rb
+++ w01/d02/Jacob/methods_exercise.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-def drinking_age(age)
+def drinking_age?(age)
   if age < 21
     puts "You are not old enough to (legally) drink yet."
     return false
@@ -11,21 +11,29 @@ def drinking_age(age)
 end
 
 def ac_action(current_temp, functional, desired_temp)
-  if functional = "yes" && current_temp > desired_temp
+  functional.downcase
+  if functional == "yes" && current_temp > desired_temp
     return "Turn on the A/C please."
-  elsif functional = "no" && current_temp > desired_temp
+  elsif functional == "no" && current_temp > desired_temp
     return "Fix the A/C now! It's hot!"
-  elsif functional = "no" && current_temp < desired_temp
+  elsif functional == "no" && current_temp < desired_temp
     return "Fix the A/C whenever you have the chance... It's cool..."
+  else
+    return "I'm comfortable."
   end
 end
 
 def is_prime?(number)
-  if number % !1 == 0 && number % !number == 0
-    return false
-  else
-    return true
+  factor = 2
+  max_potential_factor = num/2
+  while factor <= max_potential_factor
+    if (num%factor)==0
+      return false
+    end
+    factor += 1
   end
+
+  return true
 end
 
-puts is_prime?(24)
+
