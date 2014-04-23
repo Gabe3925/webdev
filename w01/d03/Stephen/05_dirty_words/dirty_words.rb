091c4032d72b697c88b466cf8ebf4f238022a2ea
stephenstanwood <stephen@stanwoodsolutions.com>
2014-04-23 16:41:07 -0400

    1.0 dirty_words.rb, deck_o_cards.rb

--- w01/d03/Stephen/05_dirty_words/dirty_words.rb
+++ w01/d03/Stephen/05_dirty_words/dirty_words.rb
@@ -1,1 +1,19 @@
-dirty_words = ['pee', 'poop', 'she-dog', 'phallus', 'fornicate', 'frack']
+# Stephen Stanwood (stephen@stanwoodsolutions.com)
+
+def ok_to_print?(words)
+  dirty_words = ['pee', 'poop', 'she-dog', 'phallus', 'fornicate', 'frack']
+
+  words.each { |w| return false if dirty_words.include? w }
+
+  return true
+end
+
+print "Go ahead, say your worst: "
+user_words = gets.chomp.split(' ')
+if(ok_to_print?(user_words))
+  puts "Looks good to us!"
+else
+  puts "Uh-oh... we have to check that out..."
+end
+
+puts
