c1f6873b4277ff91ab198cb855517cb23f284da6
yihsiaol <yihsiaol@me.com>
2014-04-23 00:40:57 -0400

    newfags can't triforce

--- w01/d02/Yi-Hsiao/pyramid.rb
+++ w01/d02/Yi-Hsiao/pyramid.rb
@@ -9,13 +9,14 @@
 # ### Bonus
 
 # * Ask the user if they want their pyramid upside-down and print it the way that they want it.
+# require "pry"
 
 def get_rows
   # prompts a user for the number of rows on their pyramid
   # expects a positive integer
   # repeat question if user enters wrong input
 
-  puts "How many rows do you want on your pyramid?"
+  puts "How many rows do you want on your triforce?"
   response = gets.chomp
   if response =~ /^[0-9]+$/ && response.to_i > 0
     response.to_i
@@ -25,36 +26,75 @@ def get_rows
   end
 end
 
-def pointy_on_top?
-  poss_answers = ["Y", "N"]
-  puts "Do you want the point on top (#{poss_answers.join(",")})?"
-  answer = gets.chomp.upcase
-  if poss_answers.include? answer
-    answer == "Y" ? true : false
-  else
-    puts "Sorry, I didn't understand you."
-    point_on_top?
+# def affirmative? question
+#   poss_answers = ["Y", "N"]
+#   puts "#{question} (Y/N)?"
+
+#   reply = gets.chomp.upcase
+#   if poss_answers.include? reply
+#     reply == poss_answers.first ? true : false
+#   else
+#     puts "Sorry, I didn't understand you."
+#     affirmative? question, poss_answers
+#   end
+# end
+
+def build_pyramid block, height, width, point_on_top
+  pyramid = []
+  height.times do |i|
+    j = point_on_top ? i : height - i - 1
+    pyramid << ( (block * (2*j + 1)).center width )
   end
+  return pyramid
 end
 
-def build_pyramid block, num_rows, point_on_top
-  max_blocks_in_row = 2*num_rows + 1
-
-  num_rows.times do |i|
-    j = point_on_top ? i : num_rows - i - 1
-    puts (block * (2*j + 1)).center max_blocks_in_row
+def find_neg_space neg_tf, neg_block
+  neg_map = []
+  neg_tf.each_with_index do |row, row_num|
+    map_row = []
+    row.chars.each_with_index do |char, position|
+      ( map_row << position ) if char == neg_block
+    end
+    neg_map << map_row
   end
+  return neg_map
+end
+
+def add_neg_space pos_tf, neg_map
+  triforce = []
+  neg_map.each_with_index do |map_row, row_num|
+    tf_row = pos_tf[row_num].chars.to_a
+    map_row.each { |i| tf_row[i] = " " }
+    triforce << tf_row.join("")
+  end
+  return triforce
+end
+
+def build_triforce block, max_height
+  # height of one piece of the triforce
+  max_height -= 1 if max_height.odd?
+  part_height = max_height / 2
+  width = 2*max_height + 1
+
+  # calculate negative space in triforce
+  neg_block = block.next
+  neg_triforce = build_pyramid neg_block, part_height, width, false
+  pos_triforce = build_pyramid block, max_height, width, true
+  neg_triforce.unshift [''] * (max_height - part_height)
+  neg_triforce.flatten!
+  neg_map = find_neg_space neg_triforce, neg_block
+
+  # add negative space to pyramid to create triforce
+  triforce = add_neg_space pos_triforce, neg_map
 end
 
 def main
-  puts "Time to make a pyramid.  Give me a character to build the pyramid with.  If more than one character is given, only the first one is used."
+  puts "Which character would you like to build your triforce with?"
   block = gets.chr
-  num_rows = get_rows
-  build_pyramid block, num_rows, pointy_on_top?
+  height = get_rows
+  # pointy_top = affirmative? "Do you want the point on top"
+  # pyramid = build_pyramid block, height, pointy_top
+  build_triforce block, height
 end
 
-main
-
-
-
-
+puts main
