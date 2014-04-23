69989c07ea674dea46e05e6240f132fbcded46f5
OKitsMikael <mgd4@njit.edu>
2014-04-22 23:04:51 -0400

    Day 2 Homework commit 4

--- w01/d02/Mikael/calculator.rb
+++ w01/d02/Mikael/calculator.rb
@@ -14,6 +14,24 @@ def calculate(operation = nil, first = nil, second = nil)
     return first ** second
   when "sqrt"
     return Math.sqrt(first)
+  when "sin"
+    return Math.sin(first)
+  when "cos"
+    return Math.cos(first)
+  when "tan"
+    return Math.tan(first)
+  when "fac"
+    if first == 0
+      return 1
+    else
+      for i in 1..first
+        result = result * (first - 1)
+      end
+    return result
+    end
+  end
+end
+
   else
     return "Sorry, that is not a valid operation."
   end
@@ -21,11 +39,11 @@ def calculate(operation = nil, first = nil, second = nil)
 end
 
 puts "Welcome to the calculator!"
-puts "You may add, subtract (sub), multiply (mul), divide (div), exponentiate (exp), or square root (sqrt)."
+puts "You may add, subtract (sub), multiply (mul), divide (div), exponentiate (exp), square root (sqrt), sine (sin), cosine (cos), tangent (tan), or factorial (fac)."
 resp = "yes"
 while resp == "yes"
   puts "Please enter an operation, followed by the first operand, followed by the second operand. [i.e. calculate(\"exp\",2,3)]"
-  puts "Would you like to perform an operation? (yes/no)"
+  puts "When you are done, enter \"no\""
   resp = gets.chomp.downcase
   if resp == "yes"
     puts "Enter your command."
