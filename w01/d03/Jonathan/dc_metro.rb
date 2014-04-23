546d480e047f1ba47c2459493f9b9267154db032
jeva33 <jonathaneva@gmail.com>
2014-04-23 19:30:29 -0400

    Your Name: Jon
    Subject Line: Part 1
    
    What Happened: Lots of time but some success

--- w01/d03/Jonathan/dc_metro.rb
+++ w01/d03/Jonathan/dc_metro.rb
@@ -1,7 +1,9 @@
-def dc_metro
-  red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judiciary Square', Union Station']
-  turquoise = ['Crystal City', 'Metro Center', 'Shaw-Howard', 'Beltwater']
-  orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', 'L\'enfant Plaza']
+require 'pry'
+
+red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judiciary Square', 'Union Station']
+turquoise = ['Crystal City', 'Metro Center', 'Shaw-Howard', 'Beltwater']
+orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', 'L\'enfant Plaza']
+
 
 dc_metro = {}
 dc_metro[:red] = red
@@ -9,5 +11,16 @@ dc_metro[:turquoise] = turquoise
 dc_metro[:orange] = orange
 
 puts "Red Line Stops"
-puts " "
-puts "#{red}"
+puts dc_metro[:red]
+puts "What stop would you like to leave from?"
+sta_start = gets.chomp
+puts "What stop would you like to go to?"
+sta_end = gets.chomp
+
+now = red.index(sta_start)
+later = red.index(sta_end)
+distance = later - now
+
+puts "You have traveled #{distance} stops."
+
+binding.pry
