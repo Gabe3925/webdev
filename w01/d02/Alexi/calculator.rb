ffa4cf16725d005fb686f07fd9336e700f05763d
alexi215 <alexi83@gmail.com>
2014-04-22 23:44:20 -0400

    calculator final draft

--- w01/d02/Alexi/calculator.rb
+++ w01/d02/Alexi/calculator.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
+run_program = true
+while run_program
 
-  puts "Calculator Menu"
+  puts "Integer Calculator Menu"
   puts "Select what type of calculations you wish to perform: "
   puts "|--Menu--||"
   puts "Select from one of the following:"
@@ -14,22 +16,24 @@ require 'pry'
   puts "0. Enter 0 to exit"
   menu_select = gets.to_i
 
-  while menu_select != 0
-    if menu_select != 5 || menu_select != 6
-      puts "What is your first number?"
-        value1 = gets.to_f
-      puts "What is your second number?"
-        value2 = gets.to_f
-    elsif menu_select == 5
-      puts "What is your base?"
-        value1 = gets.to_f
-      puts "What is your exponent?"
-        value2 = gets.to_f
-    else menu_select == 6
-      puts "What is your number?"
-        val1 = gets.to_f
-        val2 = 0.5
-    end
+  if menu_select == 0
+    puts "Goodbye!"
+    run_program = false
+  elsif menu_select == 5
+    puts "What is your base?"
+      value1 = gets.to_f
+    puts "What is your exponent?"
+      value2 = gets.to_f
+  elsif menu_select == 6
+    puts "What is your number?"
+      val1 = gets.to_i
+      #val2 = (0.5)
+  else
+    puts "What is your first number?"
+      value1 = gets.to_f
+    puts "What is your second number?"
+      value2 = gets.to_f
+  end
 
     def calc_basic(val1, val2, operation)
       if operation == 1
@@ -43,36 +47,12 @@ require 'pry'
       elsif operation == 5
         return val1 ** val2
       else operation == 6
-        return val1 * (val2)
+        #return val1 ** val2
+        return sqrt(val1)
       end
     end
     
     result = calc_basic(value1, value2, menu_select)
     puts "Your answer is #{result}"
    
-
-  # puts "Goodbye!"
-  end
 end
-
-
-  # elsif menu_select == 5 or menu_select ==6
-  #   puts "What is your base?"
-  #     base = gets.to_i
-  #   puts "What is your exponent?"
-  #     exponent = gets.to_i
-
-  #   def calc_adv(val1, val2, operation)
-  #     if operation == 5
-  #       return math.Exponents(val1)
-  #     else
-  #       return math.Square(val1)
-
-  #   result = calc_adv(value1, value2, menu_select)
-
-  # elsif menu_select = 0
-  #   
-
-
-#Test Case
-#calc_basic(5, 10)
