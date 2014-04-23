00508e8addc60bd79bc8ebcd489ac83d141c800b
richessler <richessler@gmail.com>
2014-04-23 17:12:15 -0400

    Day three updates

--- w01/d03/Richard/04_deck_o_cards/deck_o_cards.rb
+++ w01/d03/Richard/04_deck_o_cards/deck_o_cards.rb
@@ -1,7 +1,16 @@
+
+
 def deck_o_cards
   values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
   suits = ['hearts', 'diamonds', 'clubs', 'spades']
+  deck = []
 
-  # ... build your deck ...
-  
+  suits.each do |v| # loop through all values and assume entity v
+    values.each do |s| # loop through all suits and assume entity s
+          deck.push({value: v, suit: s})
+    end
+  end
+  return deck.shuffle
 end
+
+puts deck_o_cards
--- w01/d03/Richard/05_dirty_words/dirty_words.rb
+++ w01/d03/Richard/05_dirty_words/dirty_words.rb
@@ -1,1 +1,27 @@
+puts "Go ahead say your worst!"
+sentence = gets.split(', ')
+
 dirty_words = ['pee', 'poop', 'she-dog', 'phallus', 'fornicate', 'frack']
+is_dirty = false
+
+# sentence.each do |words|
+#   dirty_words.each do |bad|
+#     if words.downcase.include? bad.downcase
+#       puts "cannot print!"
+#       break
+#       end
+#   end
+# end
+
+phrase.each do |words|
+  if !is_dirty
+    is_dirty = dirty_words.include?(words)
+  end
+end
+
+if is_dirty
+  puts "No way!"
+else
+  puts "ok."
+end
+
