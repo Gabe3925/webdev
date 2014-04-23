1974eeb17d003805834c25c3ad3184f58ce487ab
Inhak Kim <inhakki@gmail.com>
2014-04-22 21:05:15 -0400

    Added square root and exponent functions to calculator.rb

--- w01/d02/Inhak/calculator.rb
+++ w01/d02/Inhak/calculator.rb
@@ -3,7 +3,7 @@ last_num = nil
 
 while in_operation == true
   puts "LA CALCULATRICE: what would you like to do?"
-  puts "add, subtract, multiply, divide, quit"
+  puts "add, subtract, multiply, divide, exponent, sqrt, quit"
   user_command = gets.chomp.downcase
 
   case user_command
@@ -32,12 +32,25 @@ while in_operation == true
     puts "number to divide"
     num1 = gets.to_f
     puts "#{num1} / __"
+    num2 = gets.to_f
     last_num = num1 / num2
-    puts lats_num
-  when quit
+    puts last_num
+  when "exponent"
+    puts "What base number do you want to POWER?"
+    num1 = gets.to_f
+    puts "#{num1}^__"
+    num2 = gets.to_f
+    last_num = num1 ** num2
+    puts last_num
+  when "sqrt"
+    puts "What number do you want to find the square root of?"
+    num1 = gets.to_f
+    last_num = Math.sqrt(num1)
+    puts last_num
+  when "quit"
     puts "Are you sure you want to quit? (Y/N)"
     yesno = gets.chomp.downcase
-    if yesno == "Y"
+    if yesno == "y"
       in_operation = false
     end
   else
