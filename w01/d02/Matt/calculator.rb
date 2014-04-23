dcdb0a1620a84693b33dd5e6ab27e755382dd79c
runtropolis <matt3r@gmail.com>
2014-04-22 22:22:51 -0400

    calculator performs basic math

---
+++ w01/d02/Matt/calculator.rb
@@ -0,0 +1,23 @@
+# method to perform calculations
+
+def calc(a, b)
+  puts "#{a} + #{b} ="
+  add = a.to_i + b.to_i
+  puts add
+
+  puts "#{a} - #{b} ="
+  sub = a.to_i - b.to_i
+  puts sub
+
+  puts "#{a} * #{b} ="
+  times = a.to_i * b.to_i
+  puts times
+
+  puts "#{a} / #{b} ="
+  div = a.to_i / b.to_i
+  return div
+end
+
+calc1 = calc("4", "2") # test method
+puts calc1 # should show the right math
+
