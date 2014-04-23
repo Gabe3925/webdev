4606e7c186ae354fa4974b84c44f39c5fdbe5831
runtropolis <matt3r@gmail.com>
2014-04-23 09:02:53 -0400

    iteration of calculator with user options, some bug

--- w01/d02/Matt/calculator.rb
+++ w01/d02/Matt/calculator.rb
@@ -1,31 +1,37 @@
 # method to perform calculations
 
 def calc(a, b)
-  puts "#{a} + #{b} ="
-  add = a.to_i + b.to_i
-  puts add
-
-  puts "#{a} - #{b} ="
-  sub = a.to_i - b.to_i
-  puts sub
-
-  puts "#{a} * #{b} ="
-  times = a.to_i * b.to_i
-  puts times
-
-  puts "#{a} / #{b} ="
-  div = a.to_i / b.to_i
-  puts div
-
-  puts "sqrt(#{a}) ="
-  exp = Math.sqrt(a.to_i)
-  puts exp
-
-  puts "#{a} ** #{b} ="
-  div = a.to_i ** b.to_i
-  puts div
+  if math_type == 1
+    puts "#{a} + #{b} ="
+    add = a.to_i + b.to_i
+    puts add
+  elsif math_type == 2
+    puts "#{a} - #{b} ="
+    sub = a.to_i - b.to_i
+    puts sub
+  elsif math_type == 3
+    puts "#{a} * #{b} ="
+    times = a.to_i * b.to_i
+    puts times
+  elsif math_type == 4
+    puts "#{a} / #{b} ="
+    div = a.to_i / b.to_i
+    puts div
+  elsif math_type == 5
+    puts "sqrt(#{a}) ="
+    exp = Math.sqrt(a.to_i)
+    puts exp
+  elsif math_type == 6
+    puts "#{a} ** #{b} ="
+    pwr = a.to_i ** b.to_i
+    puts pwr
+  return
 end
 
-calc1 = calc("64", "2") # test method
-puts calc1 # should show the right math
+puts "Give me a number to math"
+a = gets.chomp
+puts "Give me a second number to math"
+b = gets.chomp
 
+calc1 = calc(a, b)
+puts calc1
