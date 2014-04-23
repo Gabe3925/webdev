6fd8cb8f0c12eb5471c0d0a463c094e723d852c3
yihsiaol <yihsiaol@me.com>
2014-04-23 10:24:59 -0400

    simplified the code

--- w01/d03/Yi-Hsiao/days.rb
+++ w01/d03/Yi-Hsiao/days.rb
@@ -5,17 +5,12 @@
 # - The user should be asked to enter what day of the week it (for Monday - Friday)
 # - Based on their answer, print an approriate reponse (ie TGIF!)
 
-require "pry"
-
-def ask_question question
-  # ask the user a question and returns the response
-  puts question
-  answer = gets.chomp
-end
-
-def get_song_of_day day
+def get_song_of_day
   # responds to a particular day of the week
-  lyrics = case day.downcase
+  puts "What day is it, today?"
+  curr_day = gets.chomp
+
+  lyrics = case curr_day.downcase
     when "monday" then "It's just another manic Monday.  I wish it were Sunday."
     when "tuesday" then "Goodbye, Ruby Tuesday.  Who could hang a name on you?"
     when "wednesday" then "Listen to Wednesday's song.  This night you go home alone."
@@ -23,13 +18,9 @@ def get_song_of_day day
     when "friday" then "It's Friday, Friday.  Gotta get down on Friday."
     when "saturday" then "This Saturday we're gonna party all night.  One we will remember the rest of our lives."
     when "sunday" then "Sunday morning rain is falling.  Steal some covers share some skin."
+    else "Sorry, I didn't catch that.  I only speak English."
   end
   return lyrics
 end
 
-def main
-  curr_day = ask_question "What day of the week is it?"
-  puts get_song_of_day curr_day
-end
-
-main
+puts get_song_of_day
