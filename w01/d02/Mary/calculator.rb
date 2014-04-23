799b5681546caf44441a2687d355687ed3980f3e
maryhipp <maryhipp@gmail.com>
2014-04-22 20:33:03 -0400

    Commit 2 for calculator HW

--- w01/d02/Mary/calculator.rb
+++ w01/d02/Mary/calculator.rb
@@ -2,12 +2,12 @@
 another = "Y"
 
 while another == "Y"
-  puts "What calculator operation would you like to perform? Your choices are +, -, *, or /."
+  puts "What calculator operation would you like to perform? Your choices are +, -, *, /, exp, or sqrt"
   operation = gets.chomp
 
   puts "Please enter the first value you'd like to use in calculation."
   val1 = gets.to_i
-  puts "Please enter the second value you'd like to use in calculation."
+  puts "Please enter the second value you'd like to use in calculation. If calculating square root, just press enter."
   val2 = gets.to_i
 
   if operation == "+"
@@ -18,6 +18,10 @@ while another == "Y"
     puts "Answer is " + (val1 * val2).to_s
   elsif operation == "/"
     puts "Answer is " + (val1 / val2).to_s
+  elsif operation == "exp"
+    puts "Answer is " + (val1 ** val2).to_s
+  elsif operation == "sqrt"
+    puts "Answer is " + Math.sqrt(val1).to_s
   end
 
   puts "Would you like to do another calculation? Type Y to continue and N to quit."
