d5d43ed34d35d3f35534a5e3903e80800f967b87
stephenstanwood <stephen@stanwoodsolutions.com>
2014-04-23 11:42:48 -0400

    1.0 never_ending_story.rb

--- w01/d03/Stephen/02_never_ending_story/never_ending_story.rb
+++ w01/d03/Stephen/02_never_ending_story/never_ending_story.rb
@@ -1,3 +1,5 @@
+# Stephen Stanwood (stephen@stanwoodsolutions.com)
+
 story = [
   'Fry is born without a delta brainwave',
   'As a result, Fry is cryogenically frozen until 2999',
@@ -5,3 +7,18 @@ story = [
   'The spaceship hits a wormhole and crashes in 1941 Roswell, NM',
   'Fry encounters his grandmother, then inadvertently sires his father'
 ]
+
+line = 0
+
+while true
+  puts story[line]
+  print "WANT MORE (YOU KNOW YOU DO)?? "
+
+  break if gets.chomp.downcase == 'n'
+
+  if line == story.length - 1
+    line = 0
+  else
+    line += 1
+  end
+end
