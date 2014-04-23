5d4eaff8a84dbf122e4fb8323830e04219cb5f86
richessler <richessler@gmail.com>
2014-04-22 20:19:00 -0400

    Commit #3 for Calculator Homeowrk

--- w01/d02/Richard/calculator.rb
+++ w01/d02/Richard/calculator.rb
@@ -1,23 +1,58 @@
 require 'pry'
+puts "ARE YOU READY TO CALCULATE!?"
+puts "Here are a list of operations we can perform: "
+operation = "0"
+operations = [
+  "1.  Addition +",
+  "2.  Subtraction -",
+  "3.  Multiplication *",
+  "4.  Division /",
+  "5.  Exponentials ^",
+  "6.  Square Roots",
+  "7.  Sine",
+  "8.  Cosine",
+  "9.  Tangent",
+  "10. Exit Calculator"
+]
 
-
-def calculator(operation, x, y)
+def calculator(operation, n1, n2)
   case operation
     when "1"
-      puts "You've chosen Addition! #{x} + #{y} = #{x + y}"
+      return "You've chosen Addition! #{n1} + #{n2} = #{n1 + n2}"
     when "2"
-      puts "You've chosen Subtraction! #{x} - #{y} = #{x - y}"
+      return "You've chosen Subtraction! #{n1} - #{n2} = #{n1 - n2}"
     when "3"
-      puts  "You've chosen Multiplication! #{x} * #{y} = #{x * y}"
+      return  "You've chosen Multiplication! #{n1} * #{n2} = #{n1 * n2}"
     when "4"
-      puts  "You've chosen Division! #{x} / #{y} = #{x / y}"
+      return  "You've chosen Division! #{n1} / #{n2} = #{n1 / n2}"
     when "5"
-      puts "You've chosen to raise #{x} to the power of #{y} which equals #{x**y}"
+      return "You've chosen to raise #{n1} to the power of #{n2} which equals #{n1**n2}"
     when "6"
-      puts "You've chosen to get the square root of #{x} which equals #{x.to_f**(0.5)}"
+      return "You've chosen to get the square root of #{n1} which equals #{n1.to_f**n2}"
     end
-
 end
 
+run = true
+while run
+  operations.each {|x| puts x}
 
-calculator("6",3,5)
+  puts "Choose a number associated with an operator to perform:"
+
+  operation = gets.chomp
+
+  if operation == "6"
+    puts "Which number would you like to take the square root of?"
+    n1 = gets.to_i
+    n2 = 0.5
+  elsif operation == "10"
+    run = false
+  else
+    puts "What first number would you like to operate on?"
+    n1 = gets.to_i
+    puts "What about the second?"
+    n2 = gets.to_i
+  end
+  puts calculator(operation, n1, n2)
+end
+
+puts "Thanks for Calculatin'!"
