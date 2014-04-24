7caa268d39a32325d441735aa77594c5572b24d1
maryhipp <maryhipp@gmail.com>
2014-04-23 21:06:58 -0400

    DC Metro HW - red & turquoise functional but def not DRY

--- w01/d03/Mary/dc_metro.rb
+++ w01/d03/Mary/dc_metro.rb
@@ -1,40 +1,23 @@
 red = ['WOODLEY PARK', 'DUPONT CIRCLE', 'FARRAGUT NORTH', 'METRO CENTER', 'JUDICIARY SQUARE', 'UNION STATION']
 turquoise = ['CRYSTAL CITY', 'METRO CENTER', 'SHAW-HOWARD', 'BELTWATER']
-# orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', "L'enfant Plaza"]
+orange = ['FARRAGUT WEST', 'MCPHERSON SQ', 'METRO CENTER', 'FEDERAL TRIANGLE', 'SMITHSONIAN', "L'ENFANT PLAZA"]
 
 dc_metro = {}
 dc_metro[:red] = red
 dc_metro[:turquoise] = turquoise
-# dc_metro[:orange] = orange
+dc_metro[:orange] = orange
 
-puts "Welcome to DC Metro! What line would you like to get on? Options are red and turquoise."
-line_start = gets.chomp.downcase
-if line_start == "red"
-  puts "Great! The stations on the red line are " + red.join(", ") + ". Where would you like to get on?"
-elsif line_start == "turquoise"
-  puts "Great! The stations on the turquoise line are " + turquoise.join(", ") + ". Where would you like to get on?"
-end
+puts "Welcome to DC Metro! What line would you like to get on? Options are red, turquoise, and orange."
+line_start = gets.chomp.downcase.to_sym
+puts "Great! The stations on the #{line_start} line are " + dc_metro[line_start].join(", ") + ". Where would you like to get on?"
 station_start = gets.chomp.upcase
 
 puts "Okay, and which line would you like to get off at?"
-line_stop = gets.chomp.downcase
-if line_stop == "red"
-  puts "Great! The stations on the red line are " + red.join(", ") + ". Where would you like to get off?"
-elsif line_stop == "turquoise"
-  puts "Great! The stations on the turquoise line are " + turquoise.join(", ") + ". Where would you like to get off?"
-end
+line_stop = gets.chomp.downcase.to_sym
+puts "Great! The stations on the #{line_stop} line are " + dc_metro[line_stop].join(", ") + ". Where would you like to get off?"
 station_stop = gets.chomp.upcase
 
-if line_start == "red" && line_stop == "red"
-  puts "Your trip will be #{(red.index(station_start) - red.index(station_stop)).abs} stops."
-elsif line_start == "turquoise" && line_stop == "turquoise"
-  puts "Your trip will be #{(turquoise.index(station_start) - turquoise.index(station_stop)).abs} stops."
-elsif line_start == "red" && line_stop == "turquoise"
-  red_distance = (red.index(station_start) - red.index("METRO CENTER")).abs
-  turquoise_distance = (turquoise.index(station_stop) - turquoise.index("METRO CENTER")).abs
-  puts "Your trip will be #{red_distance + turquoise_distance} stop(s)."
-else
-  red_distance = (red.index(station_stop) - red.index("METRO CENTER")).abs
-  turquoise_distance = (turquoise.index(station_start) - turquoise.index("METRO CENTER")).abs
-  puts "Your trip will be #{red_distance + turquoise_distance} stop(s)."
-end
+first_leg = (dc_metro[line_start].index(station_start) - dc_metro[line_start].index("METRO CENTER")).abs
+second_leg = (dc_metro[line_stop].index(station_stop) - dc_metro[line_stop].index("METRO CENTER")).abs
+
+puts "Your trip will be #{first_leg + second_leg} stop(s)."
