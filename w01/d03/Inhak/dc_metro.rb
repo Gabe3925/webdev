5fd25d5c26494c16c545186c2bb5cc76b2b8c241
Inhak Kim <inhakki@gmail.com>
2014-04-24 00:07:50 -0400

    added downcases to arrays

--- w01/d03/Inhak/dc_metro.rb
+++ w01/d03/Inhak/dc_metro.rb
@@ -9,11 +9,23 @@ dc_metro[:red] = red
 dc_metro[:turquoise] = turquoise
 dc_metro[:orange] = orange
 
+puts "what line do you get on at? (red or turquoise)"
+
 puts "The stops on the red line are: #{red}"
 puts "What stop are you getting on at?"
 user_enter = gets.chomp
 puts "What stop do you want to get off at?"
 user_exit = gets.chomp
+
+def array_downcase(array)
+  array.each do |x|
+    x.downcase!
+  end
+end
+
+array_downcase(red)
+array_downcase(turquoise)
+array_downcase(orange)
 
 def find_station_index(line, stop)
   line.each do |x|
