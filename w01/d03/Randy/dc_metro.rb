3d5e76a2c7e4b5655506b2f3a34678d5e9af5e22
rmlatz <rmlatz@gmail.com>
2014-04-23 22:56:21 -0400

    Day 3 HW Complete, functional and way prettier than when I started

--- w01/d03/Randy/dc_metro.rb
+++ w01/d03/Randy/dc_metro.rb
@@ -1,6 +1,3 @@
-require 'pry'
-
-
 red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judiciary Square', 'Union Station']
 turquoise = ['Crystal City', 'Metro Center', 'Shaw-Howard', 'Beltwater']
 orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', "L'enfant Plaza"]
@@ -10,75 +7,55 @@ dc_metro[:red] = red
 dc_metro[:turquoise] = turquoise
 dc_metro[:orange] = orange
 
-puts "Welcome to the DC Metro -- We're Terrible!"
-red_stop_list = red.join(" <--> ")
-turquoise_stop_list = turquoise.join(" <-->")
-orange_stop_list = orange.join(" <--> ")
-
-puts "What line are you getting on? (Red, Turquoise, Orange)"
-begin_line  = gets.chomp.downcase
-	case begin_line
-		when "red"
-			puts "Red Line Stations are: #{red_stop_list}"
-			begin_line = :red
-		when "turquoise"
-			puts "Turquoise Line Stations are: #{turquoise_stop_list}"
-			begin_line = :turquoise
-		when "orange"
-			puts "Orange Line Stations are: #{orange_stop_list}"
-			begin_line = :orange
-		end
-
-puts "What's the stop you'll get on?"
-	begin_stop = gets.chomp
-	begin_local = dc_metro[begin_line].index(begin_stop)
-
-puts "What line are you getting off? (Red, Turquoise, Orange)"
-end_line = gets.chomp.downcase
-	case end_line
-		when "red"
-			puts "Red Line Stations are: #{red_stop_list}"
-			end_line = :red
-		when "turquoise"
-			puts "Turquoise Line Stations are: #{turquoise_stop_list}"
-			end_line = :turquoise
-		when "orange"
-			puts "Orange Line Stations are: #{orange_stop_list}"
-			end_line = :orange
-		end
-
-puts "What's the stop you'll getting off?"
-	end_stop = gets.chomp
-	end_local = dc_metro[end_line].index(end_stop)
-
-if begin_line != end_line && end_stop != "Metro Center"
-  trip_1_1 = dc_metro[begin_line].index(begin_stop)
-  trip_1_2 = dc_metro[begin_line].index("Metro Center")
-	if trip_1_1 < trip_1_2
-		distance_1 = trip_1_2 - trip_1_1
-	else
-		distance_1 = trip_1_1 - trip_1_2
-	end
-  trip_2_1 = dc_metro[end_line].index(end_stop)
-  trip_2_2 = dc_metro[end_line].index("Metro Center")
-  	if trip_2_1 < trip_2_2
-		distance_2 = trip_2_2 - trip_2_1
-	else
-		distance_2 = trip_2_1 - trip_2_2
-	end
-distance = (distance_1 + distance_2)
-else
-	if begin_local < end_local
-	distance = end_local - begin_local
-	else
-	distance = begin_local - end_local
-	end
+def how_far(hash, line, stop)
+	trip_1 = hash[line].index(stop)
+	trip_2 = hash[line].index("Metro Center")
+	distance = (trip_2 - trip_1).abs
+	return distance
 end
 
+def what_line(hash, line)
+  red_stop_list = hash[:red].join(" <--> ")
+  turquoise_stop_list = hash[:turquoise].join(" <--> ")
+  orange_stop_list = hash[:orange].join(" <--> ")
+	case line
+		when "red"
+			puts "Red Line Stations are: #{red_stop_list}"
+			line = :red
+		when "turquoise"
+			puts "Turquoise Line Stations are: #{turquoise_stop_list}"
+			line = :turquoise
+		when "orange"
+			puts "Orange Line Stations are: #{orange_stop_list}"
+			line = :orange
+		end
+		return line
+end
+go_again = ""
+while go_again != "n"
+	puts "Welcome to the DC Metro -- We're Terrible!"
+	puts "What line will you be entering? (Red, Turquoise, Orange)"
+	begin_line = what_line(dc_metro, gets.chomp.downcase)
 
+	puts "What's the stop you'll get on at?"
+	#call method here --
+		begin_stop = gets.chomp
+		begin_local = dc_metro[begin_line].index(begin_stop)
 
+	puts "What line are you exiting? (Red, Turquoise, Orange)"
+	end_line = what_line(dc_metro, gets.chomp.downcase)
 
-puts "There are #{distance} stops until your destination."													
+	puts "What's the stop you'll be getting off at?"
+		end_stop = gets.chomp
+		end_local = dc_metro[end_line].index(end_stop)
 
+	if begin_line != end_line && end_stop != "Metro Center"
+		distance = how_far(dc_metro, begin_line, begin_stop) + how_far(dc_metro, end_line, end_stop)
+	else
+		distance = (end_local - begin_local).abs
+	end
 
-binding.pry
+	puts "\nYou will be to your destination in #{distance} stop(s)."		
+	puts "Thank you for enduring Metro, would you like to see another trip? (y/n)"	
+	go_again = gets.chomp.downcase
+end	
