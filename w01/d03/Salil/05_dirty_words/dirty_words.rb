c449f11098c2cbb5f1722d4a4e5de61b71a6325f
4S1D2 <salolsy6@gmail.com>
2014-04-23 21:55:01 -0400

    Done

--- w01/d03/Salil/05_dirty_words/dirty_words.rb
+++ w01/d03/Salil/05_dirty_words/dirty_words.rb
@@ -5,7 +5,7 @@ puts "Go ahead, say your worst:"
 worst = gets.chomp.downcase.split
 
 worst.each do |i|
-  if (dirty_words.include?(i))
+  if dirty_words.include?(i)
     puts "We're rejecting you, potty mouth"
   else
     puts "Okay we'll publish that"
