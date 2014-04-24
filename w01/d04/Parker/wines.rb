bed34efc6a7cd1217dcf902d447210e6cc4eec8e
parkerhart <parkerhart@gmail.com>
2014-04-24 17:08:43 -0400

    Parker Hart
    Subject Line:
    
    What Happened:

---
+++ w01/d04/Parker/wines.rb
@@ -0,0 +1,26 @@
+# Write code that:
+#   - Adds a wine of your choice to the cellar
+#   - Returns a random wine from the cellar
+#   - Returns an array of just the white wines
+#   - Returns an array listing the unique types of wine
+  # - Returns an array with the all the wines that have 2-word
+
+
+wine_cellar = [
+  {:label => "Rutherford Hill", :type => "Chardonnay", :color => "white"},
+  {:label => "Nina Veneto", :type => "Pinot Grigio", :color => "white"},
+  {:label => "Wairau River", :type => "Sauvignon Blanc", :color => "white"},
+  {:label => "Tangley Oaks", :type => "Merlot", :color => "red"},
+  {:label => "Chimney Rock", :type => "Cabernet Sauvignon", :color => "red"},
+  {:label => "Sanford", :type => "Pinot Noir", :color => "red"},
+  {:label => "Alderbrook", :type => "Pinot Noir", :color => "red"},
+  {:label => "Colavita", :type => "Pinot Noir", :color => "red"},
+  {:label => "Markham", :type => "Chardonnay", :color => "white"},
+  {:label => "Angeline", :type => "Pinot Noir", :color => "red"}
+]
+
+wine_cellar.push({:label => "DC Wine", :type => "Merlot", :color => "Red"}
+)
+
+
+
