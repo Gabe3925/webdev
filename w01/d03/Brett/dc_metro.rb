51cca4203d028ef84b52a4999a9e1b62187f36d8
BYWallace <bwallace90@gmail.com>
2014-04-23 22:18:19 -0400

    The key here was the .to_sym method... Was able to figure out the math but the execution and keeping hashes/arrays straight was difficult. Happy with the solution though. Had to go with case-sensitive for stations for expediency's sake.

--- w01/d03/Brett/dc_metro.rb
+++ w01/d03/Brett/dc_metro.rb
@@ -3,7 +3,7 @@
 # 2) Woodley Park would be 3 and Beltwater would be 2
 # 3) take  value and add together to get 5 stops
 
-red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judiciary Square' 'Union Station']
+red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judiciary Square', 'Union Station']
 turquoise = ['Crystal City', 'Metro Center', 'Shaw-Howard', 'Beltwater']
 orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', "L'enfant Plaza"]
 
@@ -12,104 +12,28 @@ dc_metro[:red] = red
 dc_metro[:turquoise] = turquoise
 dc_metro[:orange] = orange
 
-count_1 = false
-count_2 = false
+#  Determine what line the rider wants to ride.
+puts "What line do you want to get on?"
+puts "- Red"
+puts "- Turquoise"
+puts "- Orange"
+line_depart = gets.chomp.downcase.to_sym
 
-while count_1 == false
-  puts "What line do you want to get on?"
-  puts "- Red"
-  puts "- Turquoise"
-  puts "- Orange"
-  start_line = gets.chomp.downcase
+puts "The stations on the #{line_depart} are " + dc_metro[line_depart].join(", ")
+puts "What station are you departing from? (case-sensitive)"
+station_depart = gets.chomp
 
-  if start_line == "red"
-    departure_key = :red
-    puts dc_metro[:red].join("\n")
-    count_1 = true
-  elsif start_line == "turquoise"
-    departure_key = :turquoise
-    puts dc_metro[:turquoise].join("\n")
-    count_1 = true
-  elsif start_line == "orange"
-    departure_key = :orange
-    puts dc_metro[:orange].join("\n")
-    count_1 = true
-  else
-    puts "That's not a metro line."
-  end
-end
+puts "What line are you getting off on?"
+puts "- Red"
+puts "- Turquoise"
+puts "- Orange"
+line_arrive = gets.chomp.downcase.to_sym
 
-start = " "
-until (dc_metro[start_key].include?(start))
-  print "Please enter the stop you will depart from: "
-  start = gets.chomp
-end
+puts "The stations on the #{line_arrive} are " + dc_metro[line_arrive].join(", ")
+puts "What station are you arriving at? (case-sensitive)"
+station_arrive = gets.chomp
 
-while count_2 == false
-  puts "What line will you finish at?"
-  puts "- Red"
-  puts "- Turquoise"
-  puts "- Orange"
-  end_line = gets.chomp.downcase
+to_metro_center = (dc_metro[line_depart].index(station_depart) - dc_metro[line_depart].index("Metro Center")).abs
+from_metro_center = (dc_metro[line_arrive].index(station_arrive) - dc_metro[line_arrive].index("Metro Center")).abs
 
-  if end_line == "red"
-    end_line = :red
-    puts dc_metro[:red].join("\n")
-    count_1 = true
-  elsif end_line == "turquoise"
-    end_line = :turquoise
-    puts dc_metro[:turquoise].join("\n")
-    count_1 = true
-  elsif end_line == "orange"
-    end_line = :orange
-    puts dc_metro[:orange].join("\n")
-    count_1 = true
-  else
-    puts "That's not a metro line."
-  end
-end
-
-stop = " "
-until (dc_metro[stop_key].include?(stop))
-  print "Please enter the stop you will arrive at: "
-  stop = gets.chomp
-end
-
-start_num = dc_metro[start.key].index(start)
-stop_num = dc_metro[stop.key].index(stop)
-
-if (start_line != stop_line)
-  start_num = ((start_num - dc_metro[start_key].index("Metro Center")).abs)
-  stop_num = ((stop_num - dc_metro[stop_key].index("Metro Center")).abs)
-  puts "Your trip is #{((stop_num-start_num).abs)} stops."
-end
-
-# # method to display menu based on line color and calculate stops
-# def line_menu(start_line)
-#   puts "Stops on this line: #{dc_metro.values}"
-
-#   puts "What stop are you departing at? (case-sensitive)"
-#   departure = gets.chomp
-
-#   puts "What stop are you arriving at? (case-sensitive)"
-#   arrival = gets.chomp
-
-#   return (start_line.index(departure) - start_line.index(arrival)).abs # the number of stops in the trip
-# end
-
-# # Determine what line the rider wants to ride.
-# puts "What line do you want to get on?"
-# puts "- Red"
-# puts "- Turquoise"
-# puts "- Orange"
-# line = gets.chomp.downcase
-
-# if line == "red"
-#     puts "Your trip is #{line_menu(red)} stops."
-#   elsif line == "turquoise"
-#     puts "Your trip is #{line_menu(turquoise)} stops."
-#   elsif line == "orange"
-#     puts "Your trip is #{line_menu(orange)} stops."
-#   else
-#     puts "That's not a metro line."
-# end
+puts "Your trip will be  #{to_metro_center + from_metro_center} stop(s)."
