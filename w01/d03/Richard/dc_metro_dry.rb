5a05efc8efd467ccaca633d6df81d8e78a08863a
richessler <richessler@gmail.com>
2014-04-24 00:06:58 -0400

    Completed Homework #3 non-dry

--- w01/d03/Richard/dc_metro_dry.rb
+++ w01/d03/Richard/dc_metro_dry.rb
@@ -4,24 +4,57 @@ red = ['Woodley Park', 'Dupont Circle', 'Farragut North', 'Metro Center', 'Judic
 turquoise = ['Crystal City', 'Metro Center', 'Shaw-Howard', 'Beltwater']
 orange = ['Farragut West', 'McPherson Sq', 'Metro Center', 'Federal Triangle', 'Smithsonian', "L'enfant Plaza"]
 
-def cap(string)
-  string.split(" ").map(&:capitalize).join(" ")
-end
+# def dist_from_metro(line)
+#   line.each_with_index do |stop, index|
+# end
+
+# red_hash =
+# {
+#     'Woodley Park' =>  3,
+#     'Dupont Circle' => 2,
+#     'Farragut North' => 1,
+#     'Metro Center'=> 0,
+#     'Judiciary Square' => 1,
+#     'Union Station' => 2
+# }
+# puts red_hash.each {|key, value| puts [key, value]}
 
 dc_metro = {}
 dc_metro[:red] = red
 dc_metro[:turquoise] = turquoise
 dc_metro[:orange] = orange
 
-colors = ["Red", "Turquoise", "Orange"]
-colors.each {|n| puts n}
+def cap(string)
+  string.split(" ").map(&:capitalize).join(" ")
+end
+
+def color_line
+  colors = ["Red", "Turquoise", "Orange"]
+  colors.each {|n| puts n}
+end
+
+def entry_stop
+  puts "Which Metro stop are you at?"
+  stop = cap(gets.chomp)
+  return
+end
+
+def entry_line (entry)
+  puts "Which Metro Station?"
+  first_stop = cap(gets.chomp)
+end
+#colors = ["Red", "Turquoise", "Orange"]
+#colors.each {|n| puts n}
+
+color_line
 puts "Which Metro line would you like to get on?"
-entry_line = gets.chomp.downcase
+entry = gets.chomp.downcase
+entry_line(entry)
+
   if entry_line == "red"
     red.each {|i| puts i}
-    puts "Which Metro stop are you at?"
-    stop = gets.chomp
-    stop = cap(stop)
+    entry_stop
+    put stop
   elsif entry_line == "turquoise"
     turquoise.each {|i| puts i}
     puts "Which Metro stop are you at?"
@@ -34,9 +67,8 @@ entry_line = gets.chomp.downcase
     stop = cap(stop)
   end
 
-colors = ["Red", "Turquoise", "Orange"]
-colors.each {|n| puts n}
-puts "Which Metro line would you like to get on?"
+color_line
+puts "Which Metro line would you like to end up on?"
 dest_line = gets.chomp.downcase
 puts "What is your destination?"
 if dest_line == "red"
