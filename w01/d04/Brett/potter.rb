072d3f8108e49e09a9d37bfd52eaaf7faa2974bb
BYWallace <bwallace90@gmail.com>
2014-04-24 23:15:18 -0400

    Defined method to return characters with over 500 mentions

--- w01/d04/Brett/potter.rb
+++ w01/d04/Brett/potter.rb
@@ -21,6 +21,22 @@ create_name_array
 
 
 # * Return an array with all the characters with more than 500 mentions. Each entry in the array should contain all information about that character. (ie `{:mentions=>18956, :name=>"Harry Potter", :house=>"Gryffindor"}`)
+
+def most_mentioned_characters
+  f = File.new("potter.csv", "r")
+  character_array = []
+  f.each_line do |character|
+    character_info = character.chomp.split(",")
+    if character_info[0].to_i > 500
+    character_array.push(character)
+   end
+  end
+  return character_array
+  f.close
+end
+
+most_mentioned_characters
+
 # * Return an array with the names of all the houses. `["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]`
 # * Return an array with all the characters with one word names. Each entry in the array should contain all information about that character. (ie `{:mentions=>1797, :name=>"Voldemort", :house=>"Slytherin"}`)
 # * Return the number of characters from Hufflepuff (Hint: It should be 3)
