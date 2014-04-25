7f9384e37fa2584e5cfa86e7d4be27cfe4da8e92
runtropolis <matt3r@gmail.com>
2014-04-25 08:59:46 -0400

    removed reference code

--- w01/d04/Matt/hw_potter.rb
+++ w01/d04/Matt/hw_potter.rb
@@ -1,28 +1,14 @@
 require "pry"
-# answer = "0"
-# puts ""
-# puts "Welcome to the jungle\n"
 
-# puts "1. If you want to add an animal"
-# puts "2. If you want to see the animals"
-# puts "3. If you want to leave"
-# answer = gets.chomp
-# if answer == "1"
-#   f = File.new("potter.csv", "a+")
-#     puts "What is the animal species?"
-#     species = gets.chomp
-#     puts "What is it\'s name?"
-#     name = gets.chomp
-#     animal = [species,name]
-#     f.puts animal.join(",")
-#   f.close
-# elsif answer == "2"
 potter_chars = []
+all = []
   f = File.new("potter.csv", "r")
     f.each_line do |line|
       potter_data = line.chomp.split(",")
 
       potter_chars.push({:mentions => potter_data[0], :name => potter_data[1], :house => potter_data[2]})
+
+
       # puts potter_chars
     end
 
@@ -30,7 +16,18 @@ potter_chars = []
     all_names = potter_chars.map do |names|
       names[:name]
     end
-    puts all_names
+
+    all_houses = potter_chars.map do |houses|
+      houses[:house]
+    end
+
+    all_mentions = potter_chars.map do |mentions|
+      mentions[:mentions]
+    end
+
+    all << all_names
+    all << all_houses
+    all << all_mentions
 
     # find characters mentioned more than 500x
     plus_500 = potter_chars.select do |num|
@@ -44,45 +41,30 @@ potter_chars = []
     end
     puts houses_list.uniq
 
+    binding.pry
+
     # find all names that are only 1 word
-    if all_names.include?(" ")
-      all_names.delete
+    long_names = all_names.include?(" ") do |long|
+
     end
 
     # find characters with one word names
     one_name = potter_chars.select do |name|
-      name[:name].include('Weasley')
+      all[0].select('Weasley')
+    end
+
+    one = all.select do |al|
+      al.has_value?("Weasley")
     end
 
 
-    # huff_names = potter_chars.map do |huff|
-    #   huff[:house].count('Hufflepuff')
-    # end
+    huff_names = potter_chars.map do |huff|
+      huff[:house].index('Hufflepuff')
+    end
 
+    all = potter_chars.map do |all|
+      all[:name]
+    end
 
     binding.pry
   f.close
-# else
-# end
-
-# chars = File.new("potter.csv", "r")
-# chars.each_line do |line|
-#   potter_data = line.chomp.split(",")
-#   puts potter_data
-#   potter_chars = []
-
-#   # char_apps = potter_data[0]
-#   # puts char_apps.each
-#   binding.pry
-# end
-  # char_apps = potter_data[0]
-  # char_name = potter_data[1]
-  # char_house = potter_data[2]
-  # puts "#{char_apps} + #{char_name} + #{char_house}"
-
-
-# chars.close
-
-# name = line.chomp.split(",")
-
-# IO.foreach("potter.csv") {|x| print x }
