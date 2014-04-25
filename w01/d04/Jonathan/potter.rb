18e243295985f6c553d16c571b554bc5f9ba8b87
jeva33 <jonathaneva@gmail.com>
2014-04-25 18:18:14 -0400

    Still working

--- w01/d04/Jonathan/potter.rb
+++ w01/d04/Jonathan/potter.rb
@@ -11,13 +11,16 @@ char_arr = []
 f.each_line do |char_string|
   char_minus_new_line = char_string.chomp
   arr_representing_char = char_minus_new_line.split(',')
+
+  binding pry
+
   character = {}
   character[:mentions] = arr_representing_char[0]
   character[:char] = arr_representing_char[1]
   character[:house] = arr_representing_char[2]
   char_arr << character
 
-binding pry
+
 
 
 end
