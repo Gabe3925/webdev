956ae87b6afd11c510fcb9d19e8bc2addb59a401
Inhak Kim <inhakki@gmail.com>
2014-04-24 21:43:27 -0400

    Finished the homework for day 4

--- w01/d04/Inhak/potter.rb
+++ w01/d04/Inhak/potter.rb
@@ -51,10 +51,64 @@ def one_word_names (file)
   puts jesus_wizards
 end
 
-def
-
+def huff_n_puff (file)
+  count = 0
+  file.each_line do |line|
+    name_data = line.chomp.split(",")
+    if name_data[2] == "Hufflepuff"
+      count += 1
+    end
+  end
+  puts count
 end
 
+def nirehtyls (file)
+  all_wizards =[]
+  file.each_line do |line|
+    name_data = line.chomp.split(",")
+    if name_data[2] == "Slytherin"
+      all_wizards.push(name_data[1].reverse)
+    else
+      all_wizards.push(name_data[1])
+    end
+  end
+  puts all_wizards
+end
+
+def gryff_families (file)
+  family = []
+  file.each_line do |line|
+    name_data = line.chomp.split(",")
+      if name_data[2] == "Gryffindor"
+        trunc_names = name_data[1].split(" ")
+        family.push(trunc_names[1])
+      end
+  end
+  puts family.uniq
+end
+
+def badger_weasley (file)
+  badger = []
+  file.each_line do |line|
+    name_data = line.chomp.split(",")
+    if name_data[1].include?("Weasley")
+      middle = name_data[1].split(" ")
+      badger.push(middle.insert(1, "Badger").join(" "))
+    end
+  end
+  puts badger
+end
+
+def namey (file)
+  nameys = []
+  file.each_line do |line|
+    name_data = line.chomp.split(",")
+    if name_data[1][-1] == "y"
+      nameys = name_data[1]
+    end
+  end
+  puts nameys
+end
 
 puts "NAMES OF PEOPLE"
 name_lister_file(File.new("potter.csv", "r"))
@@ -64,4 +118,13 @@ puts "\nHOUSES OF HOGWARTS"
 houses(File.new("potter.csv", "r"))
 puts "\nWIZARDS WITH ONE WORD NAMES"
 one_word_names(File.new("potter.csv", "r"))
-
+puts "\nNUMBER OF HUFFLEPUFF WIZARDS IN THE LIST"
+huff_n_puff(File.new("potter.csv", "r"))
+puts "\nSLYTHERINS ARE REVERSED!!!!"
+nirehtyls(File.new("potter.csv", "r"))
+puts "\nGRYFFINDOR FAMILY NAMES"
+gryff_families(File.new("potter.csv", "r"))
+puts "\nBADGERING THE FAMILY"
+badger_weasley(File.new("potter.csv", "r"))
+puts "\nNAMES ENDING WITH Y"
+namey(File.new("potter.csv", "r"))
