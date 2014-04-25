60f0bed36d56602540253c7358fa400bb6c14bda
GS888 <gsnydermail@gmail.com>
2014-04-24 22:27:24 -0400

    put data into real hash, with symbol keys. produced names only.

--- w01/d04/Gabe/potter.rb
+++ w01/d04/Gabe/potter.rb
@@ -1,12 +1,34 @@
-#potty = File.new("potter.csv", "r") 
-#potty.to_a.map.each {|row| row.to_hash }
-
-#puts potty
-animal_data = []
+								#potty = File.new("potter.csv", "r") 
+								#potty.to_a.map.each {|row| row.to_hash }
+								
+								#puts potty
+potty = {}
 f = File.new("potter.csv", "r") #read only File
 f.each_line do |line| 
-characters = line.chomp.split(", ")
-puts characters[1]
-puts characters[0]
+characters = line.chomp.split(",")
+
+mentions = characters[0]
+persons = characters[1]
+houses = characters[2]
+
+							#puts persons
+							
+							#potter_characters_hash = Hash[characters.map {|num, person, house| [num]=>num: [person]=>person: [house]=>house: }]
+							#puts "potter_characters_hash: #{potter_characters_hash.inspect}"
+							
+							#potter_characters_hash = Hash[characters.map {|value, key| [key, value]}]
+							#puts "potter_characters_hash: #{potter_characters_hash.inspect}"
+							#puts potter_characters_hash
+
+bub = Hash[numbers: mentions, dudes: persons, houses: houses]  
+puts bub[:dudes]
+
+
 end
+
 f.close
+
+								#names_only = characters.each {|num, name, house| puts name}
+								#puts names_only
+
+
