86c3e56219c18384b4882469bfcda2d1d6e3d715
BYWallace <bwallace90@gmail.com>
2014-04-28 13:28:29 -0400

    Added the ability to add apartments

--- w01/d05/Brett/the_landlord.rb
+++ w01/d05/Brett/the_landlord.rb
@@ -47,6 +47,8 @@ apt_2 = Apartment.new("1635 C St SE", 2200, 1500, 2, 1.5, ["Lara"])
 apt_3 = Apartment.new("729 Otis Pl NW", 2950, 2100, 3, 1.5, ["Mark, Paymon"])
 #The user should be presented with the following options:
 #Listing all apartments. List all of the apartments. If the apartment is unoccupied you should say something like "Apt 1A is 750 sqft and has 1 bed and 1 bath. It costs $2500 a month". If an apartment is occupied by a tenant you should say something like: Travis lives in Apt 1A.
+
+main_listing = apt_1.apt_list
 menu_choice = true
 while menu_choice != "5"
 
@@ -60,20 +62,34 @@ while menu_choice != "5"
 
   case menu_choice
   when "1"
-    puts apt_1.apt_list
+    puts main_listing
+  when "2"
+    puts "Which apt do you want to look at?"
+    apt_choice = gets.to_i + 1
+    puts main_listing[apt_choice]
+  when "3"
+    puts "Address?"
+    address = gets.chomp
+    puts "Monthly rent?"
+    monthly_rent = gets.to_i
+    puts "Square feet?"
+    sqft = gets.to_i
+    puts "Number of beds?"
+    num_beds = gets.to_i
+    puts "Number of baths?"
+    num_baths = gets.to_i
+    puts "Who's renting?"
+    renters = gets.chomp
+    main_listing.push([address, monthly_rent, sqft, num_beds, num_baths, renters])
   when "5"
     menu_choice = "5"
   end
 end
 
-#View and apartment's details. Tell the user the address, monthly_rent, sqft, num_beds, num_baths, and renters
-
 
 #Add an apartment. Make sure to get the appropriate input from the user when creating an apartment
 
 #Add a tenant to the apartment. Make sure to get the appropriate input from the user to create your person. Make sure to ask which apartment they want to live in. Add the person to that apartment. NOTE: Only two people can live in an apartment.
-
-#Quit
 
 #The program should return to the menu until the user quits
 
