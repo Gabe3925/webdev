a0c4cf1292dcd44ff544fd1a302513f0fe7fd9d6
vanderhoop <vanderhoop@me.com>
2014-04-27 21:40:43 -0400

    the shelter now takes animals from clients

--- w02/d01/INSTRUCTOR/client.rb
+++ w02/d01/INSTRUCTOR/client.rb
@@ -2,9 +2,14 @@ require_relative 'animal'
 
 class Client
   attr_accessor :name, :age
+  attr_reader :pets
   def initialize(name, age)
     @name = name
     @age = age
     @pets = []
+  end
+
+  def add_pet(pet)
+    pets << pet
   end
 end
