3d2d6ec8ecac90724905b669dfd072fc54086feb
BYWallace <bwallace90@gmail.com>
2014-04-27 23:23:50 -0400

    Instantiated three initial apartments.

--- w01/d05/Brett/the_landlord.rb
+++ w01/d05/Brett/the_landlord.rb
@@ -24,3 +24,23 @@ class Apartment
 end
 
 # PART 2 - Creating the program functionality and menu display
+
+#The program should instantiate several new apartments at the outset
+
+apt_1 = Apartment.new(201 Q St NE, 2500, 1100, 2, 2, 2)
+apt_2 = Apartment.new(1635 C St SE, 2200, 1500, 2, 1.5, 2)
+apt_3 = Apartment.new(729 Otis Pl NW, 2950, 2100, 3, 1.5, 2)
+
+#The user should be presented with the following options:
+#Listing all apartments. List all of the apartments. If the apartment is unoccupied you should say something like "Apt 1A is 750 sqft and has 1 bed and 1 bath. It costs $2500 a month". If an apartment is occupied by a tenant you should say something like: Travis lives in Apt 1A.
+
+#View and apartment's details. Tell the user the address, monthly_rent, sqft, num_beds, num_baths, and renters
+
+#Add an apartment. Make sure to get the appropriate input from the user when creating an apartment
+
+#Add a tenant to the apartment. Make sure to get the appropriate input from the user to create your person. Make sure to ask which apartment they want to live in. Add the person to that apartment. NOTE: Only two people can live in an apartment.
+
+#Quit
+
+#The program should return to the menu until the user quits
+
