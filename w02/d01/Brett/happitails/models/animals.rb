51b44539d3c578b74ccd5ca17ede22d8cd65e431
BYWallace <bwallace90@gmail.com>
2014-04-28 13:58:44 -0400

    Animals class created and tested.

---
+++ w02/d01/Brett/happitails/models/animals.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Animal
+
+  def initialize(name, species, toys)
+    @name = name
+    @species = species
+    @toys = [toys]
+  end
+
+  def name
+    return @name
+  end
+
+  def species
+    return @species
+  end
+
+  def toys
+    return @toys
+  end
+end
+
+animal_1 = Animal.new("Max", "dog", "ducky")
+puts animal_1.name
+binding.pry
