42d0540934dd209e24862da0b82bceb8ed985731
rmlatz <rmlatz@gmail.com>
2014-04-28 15:49:20 -0400

    Able to add an animal, add a client, view all animals & view all clients.

--- w02/d01/Randy/main.rb
+++ w02/d01/Randy/main.rb
@@ -5,12 +5,75 @@ require_relative 'models/Client'
 require_relative 'models/Shelter'
 
 dog1 = Animal.new("Fido", "Dog")
-dog1.toys << ("Tennis Ball")
-dog1.toys << ("Frisbee")
-totally_legit_shelter = Shelter.new("Totally Legit Animals")
-totally_legit_shelter.animals << dog1
+dog1.toys.push("Tennis Ball","Frisbee")
+cat1 = Animal.new("Fluffy", "Cat")
+cat1.toys.push("'Nip", "Dead Mouse")
 
-binding.pry
+totally_legit_shelter = Shelter.new("Totally Legit Animals")
+totally_legit_shelter.animals.push(dog1, cat1)
+
+def list_options
+puts "Please select an option:"
+puts "0 --> Quit"
+puts "1 --> Create an Animal"
+puts "2 --> Create a Client"
+puts "3 --> View a List of Animals"
+puts "4 --> View a List of Clients"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+menu_answer = ""
+puts "Welcome to #{totally_legit_shelter.name}"
+while menu_answer != "0"
+
+	list_options
+	menu_answer = gets.chomp
+	case menu_answer
+	when "1"
+		totally_legit_shelter.animals << totally_legit_shelter.create_an_animal
+	when "2"
+		totally_legit_shelter.clients << totally_legit_shelter.create_a_client
+	when "3"
+		totally_legit_shelter.list_animals
+	when "4"
+		totally_legit_shelter.list_clients
+	end
+end
+
+
+
+
+
+
 
 
 
--- w02/d01/Randy/models/Shelter.rb
+++ w02/d01/Randy/models/Shelter.rb
@@ -1,4 +1,6 @@
+
 class Shelter
+	attr_accessor :name
 	attr_accessor :animals
 	attr_accessor :clients
 	def initialize (name)
@@ -6,4 +8,76 @@ class Shelter
 		@animals = []
 		@clients = []
 	end
+
+	def has_animals?
+		if animals.size > 0
+			return true
+		else
+			return false
+		end
+	end
+
+	def has_clients?
+		if clients.size > 0
+			return true
+		else
+			return false
+		end
+	end
+
+	def list_animals
+		if self.has_animals?
+			puts "Animals:"
+			animals.each{|instance|
+				puts "#{instance.name} --> #{instance.species}"}
+		else
+			puts "There are currently no animals."
+		end
+	end
+
+	def list_clients
+		if self.has_clients?
+			puts "Clients:"
+			clients.each{ |instance|
+				puts "#{instance.name}"}
+		else
+			puts "There are currently no clients."
+		end
+	end
+
+	def create_an_animal
+	puts "What is the animals name?"
+	name = gets.chomp
+	puts "What is the animals species?"
+	species = gets.chomp
+	new_animal = Animal.new(name, species)
+	puts "Does the animal have any toys? (y/n)"
+	answer = gets.chomp
+	while answer != "n"
+		puts "What is the toy?"
+		toy = gets.chomp
+		new_animal.toys << toy
+		puts "Are there any more toys? (y/n)"
+		answer = gets.chomp
+	end
+	return new_animal
+	end
+
+	def create_a_client
+	puts "What is the clients name?"
+	name = gets.chomp
+	puts "What is the clients age?"
+	age = gets.chomp
+	new_client = Client.new(name, age)
+	puts "Does the client have any pets? (y/n)"
+	answer = gets.chomp
+	while answer != "n"
+		
+		#binding.pry
+		new_client.pets << self.create_an_animal
+		puts "Does the client have any more pets? (y/n)"
+		answer = gets.chomp
+	end
+	return new_client
+end
 end
