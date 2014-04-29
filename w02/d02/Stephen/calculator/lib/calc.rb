529f0831608fac475dc9a3182560bf13b4e7d980
stephenstanwood <stephen@stanwoodsolutions.com>
2014-04-29 15:08:20 -0400

    CALCULATOR.RB 1.0 (working calculator)

--- w02/d02/Stephen/calculator/lib/calc.rb
+++ w02/d02/Stephen/calculator/lib/calc.rb
@@ -1,2 +1,36 @@
+# Stephen Stanwood (stephen@stanwoodsolutions.com)
+
 class Calculator
+  def add(x, y)
+    x + y
+  end
+
+  def subtract(x, y)
+    x - y
+  end
+
+  def power(x, y)
+    x ** y
+  end
+
+  def sum(arr)
+    return 0 if arr.empty?
+    arr.reduce(:+)
+  end
+
+  # Accepts either two numbers or an array of several & multiplies them.
+  def multiply(arr)
+    return arr.reduce(:*)
+  end
+
+  # Calculates the factorial of a given number
+  def factorial(x)
+    return nil if x < 0
+    return 1 if x < 2
+    x * factorial(x-1)
+  end
+
+  def gcd(x,y)
+    return x.gcd(y)
+  end
 end
