8ddf50f19cbbcd488bd8421bd187b4febf852111
ef718 <elfang16@gmail.com>
2014-04-28 22:41:22 -0400

    HappiTails Commit 4

--- w02/d01/Elaine/main.rb
+++ w02/d01/Elaine/main.rb
@@ -1,4 +1,4 @@
-# require 'pry'
+require 'pry'
 
 require_relative 'models/animal'
 require_relative 'models/client'
@@ -27,33 +27,45 @@ def menu_options
   puts "2. Create a client"
   puts "3. Display all animals"
   puts "4. Display all clients"
-  puts "5. Quit"
+  puts "5. Adopt an animal"
+  puts "6. Put an animal up for adoption"
+  puts "7. Quit"
+  puts ""
 end
 
 answer = "0"
 
-while answer != "5"
+while answer != "7"
   menu_options
   answer = gets.chomp
   case answer
   when "1"
     happi_tails.add_animal
 
-
   when "2"
     happi_tails.add_client
 
   # Displays all animals
   when "3"
-
     happi_tails.list_animals
+
 
   # Displays all clients
   when "4"
-
     happi_tails.list_clients
 
   when "5"
+    puts ""
+    puts "Here are the animals available for adoption:"
+    happi_tails.list_animals
+    happi_tails.adopt_animal
+
+  when "6"
+
+    happi_tails.return_animal
+
+
+  when "7"
     puts "Thanks, and goodbye!"
   else
     puts "You didn't enter a valid selection."
--- w02/d01/Elaine/models/shelter.rb
+++ w02/d01/Elaine/models/shelter.rb
@@ -11,10 +11,6 @@ class Shelter
     return @name
   end
 
-  def name=(name)
-    @name = name
-  end
-
   def animals
     return @animals
   end
@@ -23,7 +19,7 @@ class Shelter
     return @clients
   end
 
-    def animals_list
+  def animals_list
     return @animals_list
   end
 
@@ -49,26 +45,92 @@ class Shelter
 
   # Add a client
   def add_client
-    client_name = get_answer_to("What's the client's name?")
+    client_name = get_answer_to("What's the client's full name?")
     client_age = get_answer_to("How old is the client?")
     new_client = Client.new(client_name, client_age)
     @clients_list.push(new_client)
   end
 
+  # List animals
   def list_animals
     @animals_list.each_with_index do |animal, index|
-      puts "#{animal.name} is a #{animal.species}."
+      puts "#{index + 1}) #{animal.name} is a #{animal.species}."
     end
     # Returning nil to avoid terminal output of entire array
     return nil
   end
 
+  # List clients
   def list_clients
     @clients_list.each_with_index do |client, index|
-      puts "#{client.name} is #{client.age} years old."
+      if client.pets == []
+        puts "#{index + 1}) #{client.name} is #{client.age} years old and has no pets."
+      else
+
+        puts "#{index + 1}) #{client.name} is #{client.age} years old and has #{client.pets.length} pet(s):"
+        client.pets.each do |pet|
+          puts "#{pet.name} the #{pet.species}"
+        end
+      end
     end
     # Returning nil to avoid terminal output of entire array
     return nil
+  end
+
+  # Facilitates adoption
+  def adopt_animal
+    puts ""
+    puts "Enter the number of animal you would like to adopt."
+    adoption = gets.to_i
+    index_number = adoption - 1
+
+    pet = self.animals_list[index_number]
+    animals_list.delete_at(index_number)
+
+    puts ""
+    puts "Enter your client number:"
+    self.list_clients
+
+    adopter_index = gets.to_i - 1
+
+    adopter = self.clients_list[adopter_index]
+
+    adopter.add_pet(pet)
+
+    puts ""
+    self.list_clients
+
+  end
+
+  # Facilitates putting up an animal for adoption
+  def return_animal
+    puts ""
+    puts "Enter your client number:"
+    self.list_clients
+
+    returner_index = gets.to_i - 1
+
+    returner = self.clients_list[returner_index]
+
+    puts ""
+    puts "Enter the number of the pet you would like to return:"
+
+    returner.pets.each_with_index do |pet, index|
+      puts "#{index + 1}) #{pet.name} the #{pet.species}"
+    end
+
+    returned_pet_index = gets.to_i
+    index_num = returned_pet_index - 1
+
+    pet = returner.pets[index_num]
+
+    animals_list.push(pet)
+
+    returner.return_pet(pet)
+
+    puts ""
+    self.list_clients
+
   end
 
 end
