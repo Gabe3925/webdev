78daca3bc2db36051fc430125aa636307536be43
stephenstanwood <stephen@stanwoodsolutions.com>
2014-04-29 11:49:47 -0400

    SCRABBLE.RB 1.2 (now values are symbols)

--- w02/d02/Stephen/scrabble/lib/scrabble.rb
+++ w02/d02/Stephen/scrabble/lib/scrabble.rb
@@ -2,12 +2,9 @@
 
 class ScrabbleWord
   VALUES = {
-    "a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1,
-    "f" => 4, "g" => 2, "h" => 4, "i" => 1, "j" => 8,
-    "k" => 5, "l" => 1, "m" => 3, "n" => 1, "o" => 1,
-    "p" => 3, "q" => 10, "r" => 1, "s" => 1, "t" => 1,
-    "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4,
-    "z" => 10
+    a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8,
+    k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1,
+    u: 1, v: 4, w: 4, x: 8, y: 4, z: 10
   }
 
   def initialize(word)
@@ -21,7 +18,7 @@ class ScrabbleWord
 
   # Returns the total score for the word
   def score
-    return word.chars.inject(0) { |total, c| total + VALUES[c] }
+    return word.chars.inject(0) { |total, c| total + VALUES[c.to_sym] }
   end
 
   # Returns the total score for the word, multiplied by a specified factor
