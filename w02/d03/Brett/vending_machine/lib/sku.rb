4cc8128aea0f8561cfd3a1342e841f2ea4410ca1
BYWallace <bwallace90@gmail.com>
2014-04-30 17:04:31 -0400

    Finished vending machine code lab. Still needs refactoring.

--- w02/d03/Brett/vending_machine/lib/sku.rb
+++ w02/d03/Brett/vending_machine/lib/sku.rb
@@ -1,7 +1,7 @@
 class Sku
   attr_reader :code, :product, :price, :quantity
 
-  def initialize(code, product, price, quantity)
+  def initialize(code, product="", price=0, quantity=0)
     @code = code
     @product = product
     @price = price
