a52f84287d160fe2c34c0ad26c4c2ed77a2748d6
ef718 <elfang16@gmail.com>
2014-04-30 19:21:04 -0400

    Complete HW 02-03 Updated

--- w02/d03/Elaine/gladiator/lib/arena.rb
+++ w02/d03/Elaine/gladiator/lib/arena.rb
@@ -15,26 +15,40 @@ class Arena
   end
 
   def fight
-    if @gladiators.length == 2
-      if @gladiators[0].weapon == "Trident" && @gladiators[1].weapon == "Spear"
-        @gladiators.delete_at(1)
-      elsif @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Trident"
-        @gladiators.delete_at(0)
-      elsif @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Club"
-        @gladiators.delete_at(1)
-      elsif @gladiators[0].weapon == "Club" && @gladiators[1].weapon == "Spear"
-        @gladiators.delete_at(0)
-      elsif @gladiators[0].weapon == "Club" && @gladiators[1].weapon == "Trident"
-        @gladiators.delete_at(1)
-      elsif @gladiators[0].weapon == "Trident" && @gladiators[1].weapon == "Club"
-        @gladiators.delete_at(0)
-      elsif @gladiators[0].weapon == @gladiators[1].weapon
-        @gladiators.delete_at(1)
-        @gladiators.delete_at(0)
-      else
-      end # ends weapons comparison if statement
-    elsif @gladiators.length < 2
+    # if @gladiators.length == 2
+    #   if @gladiators[0].weapon == "Trident" && @gladiators[1].weapon == "Spear"
+    #     @gladiators.delete_at(1)
+    #   elsif @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Trident"
+    #     @gladiators.delete_at(0)
+    #   elsif @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Club"
+    #     @gladiators.delete_at(1)
+    #   elsif @gladiators[0].weapon == "Club" && @gladiators[1].weapon == "Spear"
+    #     @gladiators.delete_at(0)
+    #   elsif @gladiators[0].weapon == "Club" && @gladiators[1].weapon == "Trident"
+    #     @gladiators.delete_at(1)
+    #   elsif @gladiators[0].weapon == "Trident" && @gladiators[1].weapon == "Club"
+    #     @gladiators.delete_at(0)
+    #   elsif @gladiators[0].weapon == @gladiators[1].weapon
+    #     @gladiators.delete_at(1)
+    #     @gladiators.delete_at(0)
+    #   else
+    #   end # ends weapons comparison if statement
 
+    beats = {
+      "Spear" => "Trident",
+      "Club" => "Spear",
+      "Trident" => "Club"
+    }
+    if @gladiators.length == 2
+      if @gladiators[0].weapon == @gladiators[1].weapon
+        @gladiators.delete_at(1)
+        @gladiators.delete_at(0)
+      elsif beats[@gladiators[0].weapon] == @gladiators[1].weapon
+        @gladiators.delete_at(0)
+      elsif beats[@gladiators[0].weapon] != @gladiators[1].weapon
+        @gladiators.delete_at(1)
+      end
+    else
     end
 
   end
