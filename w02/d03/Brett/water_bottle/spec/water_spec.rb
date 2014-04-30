f26f23e3affd89feece2e0504b93e4931147c2b6
BYWallace <bwallace90@gmail.com>
2014-04-30 10:39:06 -0400

    Refactored water bottle morning exercise.

--- w02/d03/Brett/water_bottle/spec/water_spec.rb
+++ w02/d03/Brett/water_bottle/spec/water_spec.rb
@@ -4,50 +4,60 @@ require_relative '../lib/water_bottle'
 describe WaterBottle do
 
   describe "::new" do
-    it "starts empty when it is created" do
-      bottle = WaterBottle.new
+
+    context "Creating a new Water Bottle" do
+      it "starts empty when it is created" do
+        bottle = WaterBottle.new
+        expect( bottle.empty? ).to be_true
+      end
+
+      it "starts with three sips when it is created" do
+        bottle = WaterBottle.new
+        expect( bottle.sips? ).to eq(0)
+      end
+    end
+  end
+
+  describe "#fill" do
+    context "filling the water bottle" do
+      let( :bottle ) { WaterBottle.new }
+      it "can be filled up with water" do
+        bottle.fill!
+        expect( bottle.empty? ).to be_false
+      end
+
+      it "has three sips after being filled" do
+        bottle.fill!
+        expect( bottle.sips? ).to eq(3)
+      end
+    end
+  end
+
+
+  describe "#dispense" do
+    context "when it's full" do
+    let( :bottle ) { WaterBottle.new }
+    before do
+      bottle.fill!
+    end
+
+    it "can dispense water" do
+      bottle.dispense!
       expect( bottle.empty? ).to be_true
     end
 
-    it "starts with three sips when it is created" do
-      bottle = WaterBottle.new
-      expect( bottle.sips? ).to eq(0)
+    it "removes one sip from the bottle" do
+      bottle.dispense!
+      expect( bottle.sips? ).to eq(2)
     end
   end
 
-  describe "#fill!" do
-    it "can be filled up with water" do
-      bottle = WaterBottle.new
-      bottle.fill!
-      expect( bottle.empty? ).to be_false
+  context "when empty" do
+    let( :bottle ) { WaterBottle.new }
+    it "doesn't dispense water" do
+      bottle.dispense!
+      expect( bottle.dispense!  ).to eq("Water bottle is empty. Can't dispense.")
     end
-
-    it "has three sips after being filled" do
-      bottle = WaterBottle.new
-      bottle.fill!
-      expect( bottle.sips? ).to eq(3)
-    end
-  end
-
-  describe "#dispense" do
-    it "can dispense water" do
-    bottle = WaterBottle.new
-    bottle.fill!
-    bottle.dispense!
-    expect( bottle.empty? ).to be_true
-  end
-
-  it "removes one sip from the bottle" do
-    bottle = WaterBottle.new
-    bottle.fill!
-    bottle.dispense!
-    expect( bottle.sips? ).to eq(2)
-  end
-
-  it "doesn't dispense if the bottle is empty" do
-    bottle = WaterBottle.new
-    bottle.dispense!
-    expect( bottle.dispense! ).to eq("Water bottle is empty. Can't dispense.")
   end
 end
 end
