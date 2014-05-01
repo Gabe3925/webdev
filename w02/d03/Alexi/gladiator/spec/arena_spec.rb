297bcb7376e748996b40b2591251e6323558062b
alexi215 <alexi83@gmail.com>
2014-05-01 09:23:24 -0400

    Committing recent changes

--- w02/d03/Alexi/gladiator/spec/arena_spec.rb
+++ w02/d03/Alexi/gladiator/spec/arena_spec.rb
@@ -25,13 +25,3 @@ describe Arena do
 end # - END Arena class
 
 
-#def add_gladiator(gladiator_instancee)
-#@gladiator_array << gladiator_instance
-
-# describe "#add_gladiator" do
-#     it "will add the specified gladiator to the arena." do
-#       sparky = Gladiator.new("Sparky", "Club")
-#       arena.add_gladiator(sparky)
-#       expect(gladiators.empty?).to be(false)
-#     end
-#   end #Describe #add_gladiator
