9028422e863a550b5e134fc795d86d916085b6be
jeva33 <jonathaneva@gmail.com>
2014-05-01 19:20:25 -0400

    Quite a few files from w02 d02+03

---
+++ w02/d03/Jonathan/gladiator/spec/gladiator_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/gladiator'
+
+describe Gladiator
+  let(:gman) do
+    Gladiator.new("New Gladiator")
+  end
+end
+
+# describe "A gladiator has a name" do
+#   it ""
+# end
