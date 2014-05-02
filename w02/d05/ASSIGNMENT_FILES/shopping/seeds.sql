d264cf2f475c4367fdb20aeb936cd91f9727b229
vanderhoop <vanderhoop@me.com>
2014-05-02 11:59:51 -0400

    added directory assignment

--- w02/d05/ASSIGNMENT_FILES/shopping/seeds.sql
+++ w02/d05/ASSIGNMENT_FILES/shopping/seeds.sql
@@ -9,21 +9,21 @@ CREATE TABLE receipts (
   buy_date date DEFAULT current_date
 );
 
-INSERT INTO receipts (store, item, number_of_item, price, buy_date) VALUES
+INSERT INTO receipts (store, item, number_of_items, price, buy_date) VALUES
   ('Sears', 'PS4', 1, 400, 'January 21 2014'),
   ('Toys R Us', 'XBox One', 1, 500, 'January 21 2014'),
   ('Toys R Us', 'TMNT Collectors Set', 1, 25, 'January 21 2014'),
-  ('Sears', 'Legos Set', 1, 40, 'January 21 2014'),
+  ('Sears', 'Lego Set', 1, 40, 'January 21 2014'),
   ('Borders', 'Blood Meridian', 3, 12, 'March 21 2014'),
   ('Borders', 'Ham on Rye', 2, 12, 'March 21 2014'),
   ('Local Book Store', 'The Last Tycoon', 1, 14, 'March 21 2014'),
   ('Macy''s', 'Button Down Shirt', 3, 28.50, 'March 22 2014'),
-  ('JC Penny''s', 'Nikes', 1, 100, 'March 23 2014'),
-  ('JC Penny''s', 'tube socks', 3, 28, 'March 23 2014'),
-  ('JC Penny''s', 'Reeboks', 1, 60, 'March 23 2014'),
-  ('JC Penny''s', 'Umbrella, Red', 1, 10.50, 'March 23 2014'),
-  ('JC Penny''s', 'Boxers 3-Pack', 1, 20.75, 'March 23 2014'),
-  ('JC Penny''s', 'TMNT bedspread', 1, 20, 'March 23 2014'),
+  ('JC Penny', 'Nikes', 1, 100, 'March 23 2014'),
+  ('JC Penny', 'tube socks', 3, 28, 'March 23 2014'),
+  ('JC Penny', 'Reeboks', 1, 60, 'March 23 2014'),
+  ('JC Penny', 'Umbrella, Red', 1, 10.50, 'March 23 2014'),
+  ('JC Penny', 'Boxer Shorts', 3, 20.75, 'March 23 2014'),
+  ('JC Penny', 'TMNT bedspread', 1, 20, 'March 23 2014'),
   ('Sears', 'Packers Jersey', 1, 50, 'March 24 2014'),
   ('Toys R Us', 'Life', 1, 25, 'March 24 2014'),
-  ('Sears', 'laptop bag', 5, 40.50, 'March 24 2014');
+  ('Sears', 'laptop bag', 19, 40.50, 'March 24 2014');
