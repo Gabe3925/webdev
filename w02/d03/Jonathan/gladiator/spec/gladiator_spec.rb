c59f0051dce2d38c80a11819e08f365ba345b565
jeva33 <jonathaneva@gmail.com>
2014-05-03 08:28:28 -0500

    lingering files

--- w02/d03/Jonathan/gladiator/spec/gladiator_spec.rb
+++ w02/d03/Jonathan/gladiator/spec/gladiator_spec.rb
@@ -1,9 +1,20 @@
-require_relative '../lib/gladiator'
+require "spec_helper"
+require_relative "../lib/gladiator"
+require_relative "../lib/arena"
 
-describe Gladiator
-  let(:gman) do
-    Gladiator.new("New Gladiator")
+describe Gladiator do
+  let( :gman ) { Gladiator.new( "Batman", "Grapling Hook" ) }
+
+  describe ":gman" do
+    it "Gladiator has a name" do
+      expect( gman.name ).to eq( "Batman" )
+    end
   end
+
+  it "A gladiator has a weapon" do
+    expect( gman.weapon ).to eq( "Grapling Hook" )
+  end
+
 end
 
 # describe "A gladiator has a name" do
