ee59432d3c13a11a617cbfe45b400e0e826a929c
rmlatz <rmlatz@gmail.com>
2014-05-05 23:12:31 -0400

    Homeword W3D1 Fully Complete w/ Bonus and a little less terrible to look at

--- w03/d01/Randy/superhero/superhero.rb
+++ w03/d01/Randy/superhero/superhero.rb
@@ -2,87 +2,80 @@ require 'pry'
 require 'pg'
 sh_conn = PG.connect(dbname: 'superheros_db', host: 'localhost')
 
+def ask_question(question)
+puts question
+answer = gets.chomp
+return answer
+end
+
 def list_all_heros(connection)
   puts connection.exec("SELECT superhero_name FROM heros;").values
 end
 
 def add_hero(conn)
-  puts "What is the Heroes name?"
-  sh_name = gets.chomp
-  puts "What is the heroes alter-ego?"
-  sh_alter = gets.chomp
-  puts "Does the hero have a cape? (y/n)"
-  cape = gets.chomp
+  cape = ask_question("Does the hero have a cape? (y/n)")
     if cape.include? "y"
       cape = true
     else
       cape = false
     end
-  puts "What's the heroes power?"
-  power = gets.chomp
-  puts "Who is the heroes arch-nemesis?"
-  nemesis = gets.chomp
-  conn.exec("INSERT INTO heros(superhero_name, alter_ego, has_cape, power, arch_nemesis) VALUES ('#{sh_name}', '#{sh_alter}', '#{cape}', '#{power}', '#{nemesis}')")
+  conn.exec("INSERT INTO heros(superhero_name, alter_ego, has_cape, power, arch_nemesis) VALUES ('#{ask_question("What is the Heroes name?")}', '#{ask_question("What is the heroes alter-ego?")}', '#{cape}', '#{ask_question("What's the heroes power?")}', '#{ask_question("Who is the heroes arch-nemesis?")}')")
 end
 
 def specific_info(conn)
-  puts "Which hero would you like to know more about?"
   puts conn.exec("SELECT superhero_name FROM heros;").values
-  hero_name = gets.chomp
-  puts conn.exec("SELECT * FROM heros WHERE superhero_name = '#{hero_name}'").values
+  puts conn.exec("SELECT * FROM heros WHERE superhero_name = '#{ask_question("Which hero would you like to know more about?")}'").values
 end
 
 def update_hero(conn)
-  puts "Which hero would you like to update?"
   puts conn.exec("SELECT superhero_name FROM heros;").values
-  hero_name = gets.chomp
-  puts "Which attribute would you like to update?"
+  hero_name = ask_question("Which hero would you like to update?")
   puts "1-Name <-> 2-Alter-Ego <-> 3-Cape <-> 4-Powers <-> 5-Nemesis"
-  answer = gets.chomp
+  answer = ask_question("Which attribute would you like to update?")
     case answer
       when "1"
-        puts "What is the updated Super Hero name?"
-        new_name = gets.chomp
-        conn.exec("UPDATE heros SET superhero_name = '#{new_name}' WHERE superhero_name = '#{hero_name}'")
+        conn.exec("UPDATE heros SET superhero_name = '#{ask_question("What is the updated Super Hero name?")}' WHERE superhero_name = '#{hero_name}'")
       when "2"
-        puts "What is the updated Alter-Ego?"
-        new_ego = gets.chomp
-        conn.exec("UPDATE heros SET alter_ego = '#{new_ego}' WHERE superhero_name = '#{hero_name}'")
+        conn.exec("UPDATE heros SET alter_ego = '#{ask_question("What is the updated Alter-Ego?")}' WHERE superhero_name = '#{hero_name}'")
       when "3"
-        puts "Does the hero have a cape? (y/n)"
-        cape = gets.chomp
-          if cape.include? "y"
+        cape = ask_question("Does the hero have a cape? (y/n)")
+          if answer.include? "y"
             cape = true
           else
             cape = false
           end
         conn.exec("UPDATE heros SET has_cape = '#{cape}' WHERE superhero_name = '#{hero_name}'")
       when "4"
-        puts "What is the updated power?"
-        new_power = gets.chomp
-        conn.exec("UPDATE heros SET power = '#{new_power}' WHERE superhero_name = '#{hero_name}'")
+        conn.exec("UPDATE heros SET power = '#{ask_question("What is the updated power?")}' WHERE superhero_name = '#{hero_name}'")
       when "5"
-        puts "Who is the updated Nemesis?"
-        new_nem = gets.chomp
-        conn.exec("UPDATE heros SET alter_ego = '#{new_nem}' WHERE superhero_name = '#{hero_name}'")
+        conn.exec("UPDATE heros SET arch_nemesis = '#{ask_question("Who is the updated Nemesis?")}' WHERE superhero_name = '#{hero_name}'")
       end
 end
 
 def remove_hero(conn)
-  puts "Which hero would you like to update?"
   puts conn.exec("SELECT superhero_name FROM heros;").values
-  hero_name = gets.chomp
-  conn.exec("DELETE FROM heros WHERE superhero_name = '#{hero_name}'")
+  conn.exec("DELETE FROM heros WHERE superhero_name = '#{ask_question("Which hero would you like to remove?")}'")
 end
+
+def no_capes!(conn)
+answer = ask_question("Are you sure you want to remove all caped crusaders from this list?").downcase
+  if answer.include? "y"
+    conn.exec("DELETE FROM heros WHERE has_cape = 'true'")
+  else
+    puts "Good Call."
+  end
+end
+
 
 response = ""
 while response != 'q'
-  puts "Please select a menu option."
+  puts "\nPlease select a menu option."
   puts "(I) Index - List all Super Heros"
   puts "(C) Add a Super Hero"
   puts "(R) View all info for a specific Super Hero"
   puts "(U) Update a Super Hero"
   puts "(D) Remove a Super Hero"
+  puts "(F) Remove all heroes who have capes"
   puts "(Q) Quit"
   response = gets.chomp.downcase
 
@@ -97,6 +90,8 @@ while response != 'q'
       update_hero(sh_conn)
     when "d"
       remove_hero(sh_conn)
+    when "f"
+      no_capes!(sh_conn)
   end
 end
 sh_conn.close
