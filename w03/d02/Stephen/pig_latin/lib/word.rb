7a080f5c53c8e1f0ae3edf39449c50fc7a7bf0bf
stephenstanwood <stephen@stanwoodsolutions.com>
2014-05-06 10:14:26 -0400

    WORD.RB 2.0 (upgraded Pig Latin morning exercise to be way less redundant)

--- w03/d02/Stephen/pig_latin/lib/word.rb
+++ w03/d02/Stephen/pig_latin/lib/word.rb
@@ -4,6 +4,7 @@ class Word
   ALPHABET = ('a'..'z').to_a
   VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
   CONSONANTS = ALPHABET - VOWELS
+  SPECIAL_STARTS = ['y', 'qu', 'squ']
 
   def initialize(original_word)
     @original_word = original_word
@@ -17,30 +18,20 @@ class Word
   def piglatinize
     orig_copy = original_word.chars
 
-    # special case for when the word starts with a 'y' (which is a consonant)
-    if orig_copy[0] == 'y'
-      orig_copy.shift
-      return orig_copy.join + "yay"
-    end
-
-    # special case for words that start with 'qu'
-    if original_word.start_with?('qu')
-      2.times { orig_copy.shift }
-      return orig_copy.join + "quay"
-    end
-
-    # special case for words that start with 'squ'
-    if original_word.start_with?('squ')
-      3.times { orig_copy.shift }
-      return orig_copy.join + "squay"
+    # check for special cases like 'y', 'qu', 'squ'...
+    SPECIAL_STARTS.each do |pattern|
+        if original_word.start_with?(pattern)
+          pattern.length.times { orig_copy.shift }
+          return orig_copy.join + pattern + "ay"
+        end
     end
 
     # if the word starts with a vowel, just add "way" and we're done
     return original_word + "way" if VOWELS.include? orig_copy[0]
 
+    # if the word stars with a consonant, keep moving through it
     while true
       first = orig_copy[0]
-
       break if !CONSONANTS.include? first
 
       # cut off the first character & add it to the end
