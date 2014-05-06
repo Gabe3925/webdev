5b109c93db7a484ed4ecf471d1da0f1934795bbb
yihsiaol <yihsiaol@me.com>
2014-05-06 10:28:42 -0400

    finished pig latin exercise

--- w03/d02/Yi-Hsiao/pig_latin/lib/word.rb
+++ w03/d02/Yi-Hsiao/pig_latin/lib/word.rb
@@ -1,9 +1,19 @@
 class Word
+  attr_reader :original_word, :pig_latin
+
   def initialize(original_word)
-    @original_word = original_word
+    @original_word = original_word.downcase
+    @pig_latin = get_pig_latin
   end
 
-  def original_word
-    return @original_word.downcase
+  def get_pig_latin
+    case @original_word[0]
+    # for words starting with vowels, add -ay suffix to the word
+    when "a", "e", "i", "o", "u" then @original_word + "ay"
+    else
+      # take the first consecutive set of consonants and move them to the end of the word
+      # then add -ay suffix to the word
+      @original_word.sub(/(^[^aeiou]+)(.+)/, '\2\1ay')
+    end
   end
 end
