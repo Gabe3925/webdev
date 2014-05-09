ecc9721436101babc92be0835c573e37928f3b3e
ef718 <elfang16@gmail.com>
2014-05-09 00:24:40 -0400

    Update 2 HW 03-04

--- w03/d04/Elaine/sinatra_calculator.rb
+++ w03/d04/Elaine/sinatra_calculator.rb
@@ -2,9 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-past_calculations = {
-  1 => "1 + 2 = 3"
-}
+past_calculations = []
+
+first_number = {}
+second_number = {}
 
 get '/' do
   "<h1>Welcome to the calculator!</h1>"
@@ -12,18 +13,39 @@ end
 
 get '/calculator' do
 
-  "<h1>Past Calculations<p></p><p>#{past_calculations[1]}</p></h1>"
+  "<h1>Past Calculations<p></p><p>" + past_calculations.join("<br>")
 end
 
 get '/calculator/:id' do
-  id = params[:id].to_i
-  past_calculations[id]
+  id = params[:id].to_i - 1
+  if id + 1 <= past_calculations.length
+    past_calculations[id]
+  else
+    "<h1>The id, #{params[:id]}, wasn't found.</h1>"
+  end
 end
 
-post '/calculator/add/:num1/:num2' do
-
-  new_key = past_calculations.keys.last + 1
-  past_calculations[new_key] = params[:id]
-
-  "#{params[:num1]} + #{params[:num2]} = #{params[:num1].to_i + params[:num2].to_i}"
+get '/calculator/add' do
+  "<h1>What would you like to add?</h1>"
 end
+
+get '/calculator/add/:number1/:number2' do
+  sum = params[:number1].to_i + params[:number2].to_i
+
+  past_calculations.push("<h1>#{params[:number1]} + #{params[:number2]} = #{sum}</h1>")
+
+  "<h1>Addition<br>#{params[:number1]} + #{params[:number2]} = #{sum}</h1>"
+end
+
+# post '/calculator/add' do
+
+#   first_new_key = first_number.keys.last + 1
+#   second_new_key = second_number.keys.last + 1
+
+#   first_number[first_new_key] = params[:number1]
+#   second_number[second_new_key] = params[:number2]
+
+#   sum = first_number.values.last.to_i + second_number.values.last.to_i
+
+#   "#{first_number.values.last} + #{second_number.values.last} = #{sum}"
+# end
