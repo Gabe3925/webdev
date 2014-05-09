7431caca5b84491a1474a29c8292285bd6d3b40d
ef718 <elfang16@gmail.com>
2014-05-09 00:49:32 -0400

    Complete HW 03-04

--- w03/d04/Elaine/sinatra_calculator.rb
+++ w03/d04/Elaine/sinatra_calculator.rb
@@ -8,18 +8,18 @@ first_number = {}
 second_number = {}
 
 get '/' do
-  "<h1>Welcome to the calculator!</h1>"
+  "<h1>Welcome to the calculator!</h1><br><h2>Add the path <i>/calculator</i> to see past calculations.<br> Enter <i>/calculator/add</i> followed by your two numbers separated by <i>/</i> to add them.<br>Do the same for <i>subtract, multply, and divide</i>.</h2>"
 end
 
 get '/calculator' do
 
-  "<h1>Past Calculations<p></p><p>" + past_calculations.join("<br>")
+  "<h1>Past Calculations:<p></p><p>" + past_calculations.join("<br>")
 end
 
 get '/calculator/:id' do
   id = params[:id].to_i - 1
   if id + 1 <= past_calculations.length
-    past_calculations[id]
+    "<h1>Historical Calculation:<br>#{past_calculations[id]}</h1>"
   else
     "<h1>The id, #{params[:id]}, wasn't found.</h1>"
   end
@@ -35,6 +35,35 @@ get '/calculator/add/:number1/:number2' do
   past_calculations.push("<h1>#{params[:number1]} + #{params[:number2]} = #{sum}</h1>")
 
   "<h1>Addition<br>#{params[:number1]} + #{params[:number2]} = #{sum}</h1>"
+end
+
+get '/calculator/subtract/:number1/:number2' do
+  difference = params[:number1].to_i - params[:number2].to_i
+
+  past_calculations.push("<h1>#{params[:number1]} - #{params[:number2]} = #{difference}</h1>")
+
+  "<h1>Subtraction<br>#{params[:number1]} - #{params[:number2]} = #{difference}</h1>"
+end
+
+get '/calculator/multiply/:number1/:number2' do
+  product = params[:number1].to_i * params[:number2].to_i
+
+  past_calculations.push("<h1>#{params[:number1]} x #{params[:number2]} = #{product}</h1>")
+
+  "<h1>Multiplication<br>#{params[:number1]} x #{params[:number2]} = #{product}</h1>"
+end
+
+get '/calculator/divide/:number1/:number2' do
+  quotient =  params[:number1].to_i / params[:number2].to_i
+  remainder = params[:number1].to_i % params[:number2].to_i
+
+  past_calculations.push("<h1>#{params[:number1]} / #{params[:number2]} = #{quotient} , Remainder: #{remainder}</h1>")
+
+  if remainder == 0
+    "<h1>Division<br>#{params[:number1]} / #{params[:number2]} = #{quotient}</h1>"
+  else
+    "<h1>Division<br>#{params[:number1]} / #{params[:number2]} = #{quotient} <br>Remainder: #{remainder}</h1>"
+  end
 end
 
 # post '/calculator/add' do
