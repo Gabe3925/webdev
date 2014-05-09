3365d47a3320a9f3142265fe932a433b9bf2813c
stephenstanwood <stephen@stanwoodsolutions.com>
2014-05-09 14:34:43 -0400

    quick commit

--- w03/d05/Stephen/99_bottles_of_beer/server.rb
+++ w03/d05/Stephen/99_bottles_of_beer/server.rb
@@ -1,2 +1,8 @@
+#!/usr/bin/ruby
+# @Author: stephenstanwood
+# @Date:   2014-05-09 14:24:54
+# @Last Modified by:   stephenstanwood
+# @Last Modified time: 2014-05-09 14:32:28
+
 require 'sinatra'
 require 'sinatra/reloader'
