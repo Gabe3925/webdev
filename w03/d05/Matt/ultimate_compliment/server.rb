8b5aafe323cb3e79ba144022032f07bbc7ac8a9b
runtropolis <matt3r@gmail.com>
2014-05-10 16:31:58 -0400

    added post functionality

--- w03/d05/Matt/ultimate_compliment/server.rb
+++ w03/d05/Matt/ultimate_compliment/server.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-  @@compliments = [
-  "Your instructors love you",
-  "High five = ^5",
-  "Is it Ruby Tuesday yet?",
-  "It's almost beer o'clock",
-  "The Force is strong with you"
-  ]
+@@compliments = [
+"Your instructors love you",
+"High five = ^5",
+"Is it Ruby Tuesday yet?",
+"It's almost beer o'clock",
+"The Force is strong with you"
+]
 
-  @@colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+@@colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
 get "/" do
   erb :compliment
@@ -18,4 +18,9 @@ end
 get '/:name' do
   @name = params[:name]
   erb :name
+end
+
+post "/" do
+  new_compliment = params[:new_compliment]
+  @@compliments << new_compliment
 end
