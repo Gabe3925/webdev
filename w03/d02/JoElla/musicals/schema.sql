8f0886402641441a1ac2d4ce8a549c1fad237dad
runtropolis <matt3r@gmail.com>
2014-05-10 15:06:13 -0400

    initial commit of ultimate_compliment

--- w03/d02/JoElla/musicals/schema.sql
+++ w03/d02/JoElla/musicals/schema.sql
@@ -1,3 +1,7 @@
+drop table if exists performances;
+drop table if exists characters;
+drop table if exists musicals;
+drop table if exists songs;
 
 CREATE TABLE musicals (
   id serial primary key,
@@ -5,4 +9,21 @@ CREATE TABLE musicals (
   year integer not null,
   composer varchar(100) not null,
   lyricist varchar(100) not null
+);
+
+CREATE TABLE songs (
+  id serial primary key,
+  title varchar(50) not null,
+  musical_id integer references musicals(id)
+);
+
+CREATE TABLE characters (
+  id serial primary key,
+  name varchar(100) not null
+);
+
+create table performances(
+  id serial primary key,
+  song_id integer references songs(id),
+  character_id integer references characters(id)
 );
