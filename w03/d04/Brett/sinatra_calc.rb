61c0ca8a621f943c324c3075cdd72046181ebd85
BYWallace <bwallace90@gmail.com>
2014-05-10 12:51:30 -0400

    Added storage of past calculations and ability to look them up.

--- w03/d04/Brett/sinatra_calc.rb
+++ w03/d04/Brett/sinatra_calc.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/calculator' do
+# store previous calculations in array
+calculations = [4]
+
+get '/' do
   return "Welcome to the calculator!"
+end
+
+get '/calculator' do
+  return "No calculations yet!" if calculations.count == 0
+  return calculations.join(", ")
+end
+
+get '/calculator/:calc_id' do
+  return "Calculation doesn't exist!" if calculations.at(params[:calc_id].to_i - 1 ) == nil
+  return calculations.join(", ")
 end
