757141ab94c882395eea496716e91b9118204f6b
BYWallace <bwallace90@gmail.com>
2014-05-10 13:11:53 -0400

    Completed barebones Sinatra calculator

--- w03/d04/Brett/sinatra_calc.rb
+++ w03/d04/Brett/sinatra_calc.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 # store previous calculations in array
-calculations = [4]
+calculations = []
 
 get '/' do
   return "Welcome to the calculator!"
@@ -16,4 +16,28 @@ end
 get '/calculator/:calc_id' do
   return "Calculation doesn't exist!" if calculations.at(params[:calc_id].to_i - 1 ) == nil
   return calculations.join(", ")
+end
+
+post '/calculator/add/' do
+  add_answer = (params[:first_number].to_i + params[:second_number].to_i).to_s
+  calculations.push({id: params[:id], first_number: params[:first_number], second_number: params[:second_number], operation: "+"})
+  return params[:first_number].to_s + " + " + params[:second_number].to_s + " = " + add_answer.to_s
+end
+
+post '/calculator/subtract/' do
+  subtract_calc = (params[:first_number].to_i - params[:second_number].to_i).to_s
+  calculations.push({id: params[:id], first_number: params[:first_number], second_number: params[:second_number], operation: "+"})
+  return params[:first_number].to_s + " - " + params[:second_number].to_s + " = " + sbutract_answer.to_s
+end
+
+post '/calculator/multiply/' do
+  multiply_calc = (params[:first_number].to_i * params[:second_number].to_i).to_s
+  calculations.push({id: params[:id], first_number: params[:first_number], second_number: params[:second_number], operation: "+"})
+  return params[:first_number].to_s + " * " + params[:second_number].to_s + " = " + multiply_calc.to_s
+end
+
+post '/calculator/divide/' do
+  divide_calc = (params[:first_number].to_i / params[:second_number].to_i).to_s
+  calculations.push({id: params[:id], first_number: params[:first_number], second_number: params[:second_number], operation: "+"})
+  return params[:first_number].to_s + " / " + params[:second_number].to_s + " = " + divide_calc.to_s
 end
