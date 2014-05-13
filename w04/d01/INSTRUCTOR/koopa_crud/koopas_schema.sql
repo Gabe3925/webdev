a00a4399d0b00a9f00dcf4ad6b01450bd69383c3
vanderhoop <vanderhoop@me.com>
2014-05-13 14:29:07 -0400

    removed dr

--- w04/d01/INSTRUCTOR/koopa_crud/koopas_schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS koopas;
-
-CREATE TABLE koopas (
-  id serial PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  color VARCHAR(50) NOT NULL,
-  has_shell BOOLEAN DEFAULT TRUE,
-  photo_url VARCHAR(255)
-);
