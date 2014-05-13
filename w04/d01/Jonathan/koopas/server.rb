0b2a96608648a1b7b89bd1ad277591b6456ff9cf
jeva33 <jonathaneva@gmail.com>
2014-05-13 09:03:27 -0400

    Redux

--- w04/d01/Jonathan/koopas/server.rb
+++ w04/d01/Jonathan/koopas/server.rb
@@ -8,46 +8,46 @@ require_relative './models/ar_koopas'
 
 # Koopa.create(name: "Koopa Troopa", color: "yellow", has_shell: true, image_url: "http://upload.wikimedia.org/wikipedia/en/7/75/Koopa_Troopa_3D_Land.png")
 
-get '/' do
-  erb :home
-end
+# get '/' do
+#   erb :home
+# end
 
 # List all koopas
-get '/koopa_pages' do
+get '/' do
   @koopa = Koopa.all
-  erb :'koopa_pages/index'
+  erb :index
 end
 
 # New Koopa
-get '/koopa_pages/new' do
-  erb :'koopa_pages/new'
+get '/new' do
+  erb :new
 end
 
 # Create Koopa
-post '/koopa_pages' do
+post '/' do
   koopa = Koopa.create({
     name: params[:name],
     color: params[:color],
     has_shell: (params[:has_shell] || true),
     image_url: params[:image_url]
   })
-  redirect to ('/koopa_pages/' + koopa.id.to_s)
+  redirect to ('/' + koopa.id.to_s)
 end
 
 # Show Koopa
-get '/koopa_pages/:id' do
+get '/:id' do
   @koopa = Koopa.find(params[:id])
-  erb :'koopa_pages/show'
+  erb :show
 end
 
 # Edit Koopa
-get 'koopa_pages/:id/edit' do
+get ':id/edit' do
   @koopa = Koopa.find(params[:id])
-  erb :'koopa_pages/edit'
+  erb :edit
 end
 
 # Update Koopa
-post '/koopa_pages/:id' do
+post '/:id' do
   koopa = Koopa.find(params[:id])
   koopa.update({
     name: params[:name],
@@ -55,11 +55,11 @@ post '/koopa_pages/:id' do
     has_shell: (params[:has_shell] || true),
     image_url: params[:image_url]
   })
-  redirect to ('/koopa_pages/' + koopa.id.to_s)
+  redirect to ('/' + koopa.id.to_s)
 end
 # Destroy Koopa
-delete '/koopa_pages/:id' do
+delete '/:id' do
   koopa = Koopa.find(params[:id])
   koopa.destroy
-  redirect to('/koopa_pages/')
+  redirect to('/')
 end
