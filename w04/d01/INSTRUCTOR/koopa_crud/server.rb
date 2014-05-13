dc8b06e27f082c11e5642b378a73b9dc7a0dfac2
vanderhoop <vanderhoop@me.com>
2014-05-13 11:23:21 -0400

    added seeds

--- w04/d01/INSTRUCTOR/koopa_crud/server.rb
+++ w04/d01/INSTRUCTOR/koopa_crud/server.rb
@@ -5,7 +5,8 @@ require 'active_record'
 
 require_relative 'config.rb'
 
+# root should display all koopas, therefore we need all the koopas
 get '/' do
-
-
+  @koopas = Koopa.all
+  erd :index
 end
