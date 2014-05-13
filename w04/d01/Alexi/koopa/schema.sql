10e6a27f3554ef7ef134e97a73e71915e47e95a7
alexi215 <alexi83@gmail.com>
2014-05-12 23:06:26 -0400

    Koopa Homework

--- w04/d01/Alexi/koopa/schema.sql
+++ w04/d01/Alexi/koopa/schema.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS koopa;
+DROP TABLE IF EXISTS koopas;
 
 -- Run the follwing command inside /koopa root direcotry:
 -- psql -d koopa_db -f schema.sql
 
-CREATE TABLE koopa(
+CREATE TABLE koopas(
   id serial primary key,
   name varchar(50),
-  color text,
-  has_shell boolean default true,
+  color varchar(20),
+  has_shell boolean,
   photo_url text
 )
--- w04/d01/Alexi/koopa/server.rb
+++ w04/d01/Alexi/koopa/server.rb
@@ -2,33 +2,36 @@ require 'pry'
 require 'active_record'
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative './models/koopa'
+
 require_relative './models/config'
+require_relative './models/koopa'
 
 get '/' do
   erb :home
 end
 
-get '/koopa' do
-  @koopa = Koopa.all
-  erb :index
+# Display all Koopas
+get '/koopas' do
+  @koopas = Koopa.all
+  erb :'koopas/index'
 end
 
-get '/koopa/new' do
-  erb :'koopa/new'
+# Display new Koopa form
+get '/koopas/new' do
+  erb :'koopas/new'
 end
 
-post '/koopa' do
-  koopa = Koopas.create({
+post '/koopas' do
+  koopa = Koopa.create({ # Sinatra is telling me Koopa is unitialized...
     name: params[:name],
     color: params[:color],
     has_shell: (params[:has_shell] || false),
     image_url: params[:image_url]
   })
-  # redirect to('/koopa/' + koopa.id.to_s)
+  redirect to('/koopas/' + koopa.id.to_s)
 end
 
 # get '/edit/:id/edit' do
 #   @koopa = Koopa.find(params[:id])
-#   erb :'koopa/edit'
+#   erb :'koopas/edit'
 # end
