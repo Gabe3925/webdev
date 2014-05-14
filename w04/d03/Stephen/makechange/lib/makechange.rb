3cf2f189b5c2011561b3e9ae3bcb6f4b88300699
stephenstanwood <stephen@stanwoodsolutions.com>
2014-05-14 11:28:36 -0400

    updated nyan cat formatter

--- w04/d03/Stephen/makechange/lib/makechange.rb
+++ w04/d03/Stephen/makechange/lib/makechange.rb
@@ -2,19 +2,20 @@
 # @Author: stephenstanwood
 # @Date:   2014-05-14 09:04:23
 # @Last Modified by:   stephenstanwood
-# @Last Modified time: 2014-05-14 09:32:20
+# @Last Modified time: 2014-05-14 10:05:07
 
 COINS = [ :quarters, :dimes, :nickels, :pennies ]
 COIN_VALUES = { quarters: 25, dimes: 10, nickels: 5, pennies: 1 }
 
 # Makes change with the fewest number of coins for a specified # of cents
 def makechange(cents)
+  return "Enter a positive integer!" if cents.to_i != cents || cents < 0
+
   change = { quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
 
   COINS.each do |coin|
-    num = cents / COIN_VALUES[coin]
-    change[coin] += num if num > 0
-    cents -= (COIN_VALUES[coin] * num)
+    change[coin] += cents / COIN_VALUES[coin]
+    cents %= COIN_VALUES[coin]
   end
 
   return change
--- w04/d03/Stephen/makechange/spec/makechange_spec.rb
+++ w04/d03/Stephen/makechange/spec/makechange_spec.rb
@@ -2,12 +2,20 @@
 # @Author: stephenstanwood
 # @Date:   2014-05-14 09:04:53
 # @Last Modified by:   stephenstanwood
-# @Last Modified time: 2014-05-14 09:25:35
+# @Last Modified time: 2014-05-14 09:50:42
 
 require_relative "spec_helper.rb"
 require_relative "../lib/makechange.rb"
 
 describe "#makechange" do
+  it "returns an error for non-integer cents" do
+    expect(makechange("cat")).to eq "Enter a positive integer!"
+  end
+
+  it "returns an error for negative integer cents" do
+    expect(makechange(-5)).to eq "Enter a positive integer!"
+  end
+
   it "returns correct change for 50 cents" do
     c = makechange(50)
     expect(c[:quarters]).to eq 2
@@ -22,5 +30,13 @@ describe "#makechange" do
     expect(c[:dimes]).to eq 2
     expect(c[:nickels]).to eq 0
     expect(c[:pennies]).to eq 4
+  end
+
+    it "returns correct change for 95 cents" do
+    c = makechange(95)
+    expect(c[:quarters]).to eq 3
+    expect(c[:dimes]).to eq 2
+    expect(c[:nickels]).to eq 0
+    expect(c[:pennies]).to eq 0
   end
 end
