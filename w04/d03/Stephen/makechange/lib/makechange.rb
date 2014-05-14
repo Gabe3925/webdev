c864d2e62b5d0e776afe7ab83485500cc4e8396a
stephenstanwood <stephen@stanwoodsolutions.com>
2014-05-14 09:30:29 -0400

    MAKECHANGE 2.0 (works, far more elegant)

--- w04/d03/Stephen/makechange/lib/makechange.rb
+++ w04/d03/Stephen/makechange/lib/makechange.rb
@@ -2,28 +2,21 @@
 # @Author: stephenstanwood
 # @Date:   2014-05-14 09:04:23
 # @Last Modified by:   stephenstanwood
-# @Last Modified time: 2014-05-14 09:11:42
+# @Last Modified time: 2014-05-14 09:28:59
 
-#
+COINS = [ :quarters, :dimes, :nickels, :pennies ]
+COIN_VALUES = { quarters: 25, dimes: 10, nickels: 5, pennies: 1 }
 
+# Makes change with the fewest number of coins for a specified # of cents
 def makechange(cents)
-  coins = { quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+  change = { quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
 
-  while cents / 25 > 0
-    coins[:quarters] += 1
-    cents -= 25
+  COINS.each do |coin|
+    while cents / COIN_VALUES[coin] > 0
+      change[coin] += 1
+      cents -= COIN_VALUES[coin]
+    end
   end
 
-  while cents / 10 > 0
-    coins[:dimes] += 1
-    cents -= 10
-  end
-
-  while cents / 5 > 0
-    coins[:nickels] += 1
-    cents -= 5
-  end
-
-  coins[:pennies] += cents
-  return coins
+  return change
 end
--- w04/d03/Stephen/makechange/spec/makechange_spec.rb
+++ w04/d03/Stephen/makechange/spec/makechange_spec.rb
@@ -2,20 +2,25 @@
 # @Author: stephenstanwood
 # @Date:   2014-05-14 09:04:53
 # @Last Modified by:   stephenstanwood
-# @Last Modified time: 2014-05-14 09:16:19
+# @Last Modified time: 2014-05-14 09:25:35
 
 require_relative "spec_helper.rb"
 require_relative "../lib/makechange.rb"
 
 describe "#makechange" do
-  it "returns two quarters for 50 cents" do
+  it "returns correct change for 50 cents" do
     c = makechange(50)
     expect(c[:quarters]).to eq 2
+    expect(c[:dimes]).to eq 0
+    expect(c[:nickels]).to eq 0
+    expect(c[:pennies]).to eq 0
   end
 
-  it "returns 4 cents for 99 cents" do
+  it "returns correct change for 99 cents" do
     c = makechange(99)
+    expect(c[:quarters]).to eq 3
+    expect(c[:dimes]).to eq 2
+    expect(c[:nickels]).to eq 0
     expect(c[:pennies]).to eq 4
   end
-
 end
