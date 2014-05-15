c9a2deb70fb1c5de4c0b52356a708ee7ea29bbe6
yihsiaol <yihsiaol@me.com>
2014-05-15 14:52:43 -0400

    added toy feature to dog shelter

--- w04/d03/Yi-Hsiao/dogs/app/controllers/dogs_controller.rb
+++ w04/d03/Yi-Hsiao/dogs/app/controllers/dogs_controller.rb
@@ -10,18 +10,6 @@ class DogsController < ApplicationController
   def create
     @dog = Dog.new(dog_params)
 
-    uploaded_image = params[:dog][:image]
-    binding.pry
-    # image_extension = uploaded_image.original_filename.split(".")[-1]
-
-
-
-    # File.open(Rails.root.join("assets", "images", "wb")) do |image|
-    #   image.write(uploaded_image.read)
-    # end
-    
-    # if dog record is created, show user the newly created dog
-    # if the dog doesn't meet requirements, redirect users back to form
     if @dog.save
       redirect_to @dog
     else
@@ -35,6 +23,6 @@ class DogsController < ApplicationController
 
   private
     def dog_params
-      params.require(:dog).permit(:name, :age)
+      params.require(:dog).permit(:name, :age, :image)
     end
 end
--- w04/d03/Yi-Hsiao/dogs/db/schema.rb
+++ w04/d03/Yi-Hsiao/dogs/db/schema.rb
@@ -11,15 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140514205756) do
+ActiveRecord::Schema.define(version: 20140515180748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "dogs", force: true do |t|
     t.string  "name"
-    t.text    "image"
+    t.text    "image_url"
     t.integer "age"
+  end
+
+  create_table "toys", force: true do |t|
+    t.string   "name"
+    t.integer  "dog_id"
+    t.text     "photo_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
