d8bf3a2ef6224a513dc1c9618ca2fc8006527182
alexi215 <alexi83@gmail.com>
2014-05-16 16:56:11 -0400

    starting weekend homework

---
+++ w04/d05/Alexi/band_io/band/app/controllers/venues_controller.rb
@@ -0,0 +1,9 @@
+class VenuesController < ApplicationController
+
+  def index
+    @venues = Venue.all
+    render :index
+  end
+
+
+end
--- w04/d05/Alexi/superhero/app/controllers/heros_controller.rb
+++ w04/d05/Alexi/superhero/app/controllers/heros_controller.rb
@@ -14,6 +14,7 @@ class HerosController < ApplicationController
   end
 
   def edit
+    @hero = Hero.
   end
 
   def update
