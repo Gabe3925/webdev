78b796a3eb26670f7186133a7f4b8a35cb51109f
BYWallace <bwallace90@gmail.com>
2014-06-02 11:11:02 -0400

    Intro to JavaScript

---
+++ w07/d01/Brett/ruby_code.rb
@@ -0,0 +1,40 @@
+# Ruby
+# This is a Ruby comment
+
+# Variables
+beep = 'beep'
+
+# Console Output
+puts beep
+
+# Conditionals
+number = 10
+if number > 5
+  puts 'BIG NUMBER!'
+end
+elsif number < 5
+  puts 'little number'
+end
+else
+  puts 'must be five then'
+end
+
+#Case Statements
+day = 'Monday'
+case day
+when 'Monday'
+  puts 'Get haircut!'
+when 'Tuesday'
+  puts 'Dont get haircut'
+when 'Friday'
+  puts 'get three haircuts'
+else
+  puts "I'm in limbo"
+end
+
+#While Loop
+count = 10
+while count > 0
+  puts count
+  count -= 1
+end
