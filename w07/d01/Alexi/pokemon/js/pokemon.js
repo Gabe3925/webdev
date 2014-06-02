ec5c558de58188d0f2b0ff2b2a891e57dae5877f
alexi215 <alexi83@gmail.com>
2014-06-02 17:33:29 -0400

    classwork completed

--- w07/d01/Alexi/pokemon/js/pokemon.js
+++ w07/d01/Alexi/pokemon/js/pokemon.js
@@ -3,10 +3,17 @@ var pokemon = {
   pokemonList : allPokemon,
 
   printAllPokemonNamesToConsole : function(){
+    _.each(pokemon.pokemonList, function(monster){
+      console.log(monster.name);
+    });
   },
+  //--> pokemon.printAllPokemonNamesToConsole()
 
   findPokemonByName : function(name){
-  },
+    var match = _.find(pokemon.pokemonList, function(monster) {
+    });
+    return match;
+  },  
 
   findStrongestPokemon : function(){
   },
