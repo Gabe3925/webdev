bf70429c63027fe472308e649a206ba7991f94e1
ab75173 <adam.p.barrett@gmail.com>
2014-06-02 11:24:09 -0400

    updated morning work

---
+++ w07/d01/Adam/ruby_code.rb
@@ -0,0 +1,40 @@
+#Ruby
+#This is a Ruby comment
+
+# Variable
+beep = 'beep'
+
+#Console Output
+puts beep
+
+# Conditionals
+number = 5
+if number > 5
+  puts "Big Number!"
+  elsif number < 5
+  puts "little number"
+else
+  puts 'must be 5 then'
+end
+
+# Case Statements
+day ='friday'
+case day
+when 'Monday'
+  puts 'get Haircut'
+when 'Tuesday'
+  puts 'dont get a haircut'
+when 'Friday'
+  puts 'get 3 haricut'
+else
+  puts 'im in limbo'
+end
+
+# While loop
+
+count = 10
+while count > 0
+  puts count
+  count -= 1
+end
+
