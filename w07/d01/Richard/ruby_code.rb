fe2e2c843d586960d83d5b9bcb0a8824f259396a
richessler <richessler@gmail.com>
2014-06-02 12:41:14 -0400

    initial JavaScript exercises

---
+++ w07/d01/Richard/ruby_code.rb
@@ -0,0 +1,43 @@
+# Ruby
+# This is a ruby comment
+
+# Variables
+beep = 'beep'
+
+# Console Output
+puts beep
+
+# Conditionals
+num = 3
+
+if num > 5
+  puts "BIG NUMBER!"
+elsif num < 5
+  puts "little number"
+else
+  puts "must be 5 then"
+end
+
+
+# Case Statements
+day = "Salil"
+
+case day
+when "Monday"
+  puts "Get Haircut!"
+when "Tuesday"
+  puts "Don't get a haircut"
+when "Friday"
+  puts "Get three haircuts"
+else
+  puts "I'm in Haircut limbo!"
+end
+
+
+# While loop
+
+count = 10
+while count > 0
+  puts count
+  count -= 1
+end
