975108c3e49789fa7715442ee3f64c23633c847d
alexi215 <alexi83@gmail.com>
2014-06-02 11:12:11 -0400

    Morning Exercises

---
+++ w07/d01/Alexi/ruby_code.rb
@@ -0,0 +1,36 @@
+# Ruby
+# 
+
+# Variables
+beep = "beep" 
+
+# Console Output
+puts beep
+
+# Conditionals
+number = 10
+if number > 5
+  puts "Big number"
+elsif number < 5
+  puts "Little number"
+end
+
+# Case Statements
+day = "Monday"
+case day
+when "Monday"
+  puts "Get haircut!"
+when "Tuesday"
+  puts "don't get a haircut"
+when "Friday"
+  puts "Drink a beer"
+else
+  puts "Im in limbo"
+end
+
+# While Loop
+count = 10
+while count > 0
+  puts count
+  count -= 1
+end
