ca2b6206f6fefc82a4d24e11dfcdc0999153e63b
maryhipp <maryhipp@gmail.com>
2014-06-04 16:55:30 -0400

    Can draw picture

--- w07/d03/Mary/pixart/pixart.js
+++ w07/d03/Mary/pixart/pixart.js
@@ -9,7 +9,7 @@ function setColor() {
 setColor();
 
 function createSquares() {
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 8000; i++) {
     var square = document.createElement('div');
     document.body.appendChild(square);
     square.classList.add('square');
@@ -17,21 +17,21 @@ function createSquares() {
 }
 createSquares();
 
-function colorizeSquaresToGreen() {
-  squares = document.querySelectorAll('.square')
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', function() {
-      this.style.backgroundColor = 'green'
-    });
-  };
-}
-colorizeSquaresToGreen();
+// function colorizeSquaresToGreen() {
+//   squares = document.querySelectorAll('.square')
+//   for (var i = 0; i < squares.length; i++) {
+//     squares[i].addEventListener('click', function() {
+//       this.style.backgroundColor = 'green'
+//     });
+//   };
+// }
+// colorizeSquaresToGreen();
 
 function colorizeSquaresToInput() {
   squares = document.querySelectorAll('.square');
   for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', function() {
-      var color = document.getElementById('color-field').value;
+    squares[i].addEventListener('mouseover', function() {
+      var color = document.getElementById('color-field').value || 'blue';
       this.style.backgroundColor = color;
     });
   };
