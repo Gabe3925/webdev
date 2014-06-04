f45fc8525ef6e8448331f26e245096f124ff70e4
BYWallace <bwallace90@gmail.com>
2014-06-04 17:14:08 -0400

    Selected color changes square now

--- w07/d03/Brett/pixart/pixart.js
+++ w07/d03/Brett/pixart/pixart.js
@@ -2,7 +2,7 @@ function setColor() {
   var form = document.getElementById("color-form");
   form.addEventListener("submit", function(event) {
     event.preventDefault();
-    var color = document.getElementById("color-field").value;
+    color = document.getElementById("color-field").value;
     var brush = document.querySelector(".brush");
     brush.style.backgroundColor = color;
   });
@@ -14,9 +14,11 @@ function addSquares() {
     square = document.createElement("div");
     square.classList.add("square");
     square.addEventListener("click", function() {
-      this.style.backgroundColor = "green";
+      this.style.backgroundColor = color;
     });
     document.body.appendChild(square);
   }
 }
 addSquares();
+
+
