07e9d6397c53d3f586764e4626261d1d579cfb13
GS888 <gsnydermail@gmail.com>
2014-06-04 19:40:16 -0400

    changes

--- w07/d03/Gabe/pixart/pixart.js
+++ w07/d03/Gabe/pixart/pixart.js
@@ -1,11 +1,29 @@
 function brushBoxChanger() {
-  form = document.getElementById('form');
-  form.addEventListener('submit', function(event){
-    event.preventDefault();
-    var color = document.getElementById('color-field').value;
-    var colorbox = document.querySelector('.brush');
-    colorbox.style.backgroundColor = color;
-  })
+  var form = document.getElementById('form');
+  var input = document.querySelector('#color-field');
+  var brush = document.querySelector('.brush');
+
+ form.addEventListener('submit', function(event){
+  event.preventDefault();
+  brush.style.backgroundColor = input.value;
+ });
 
 }
 brushBoxChanger();
+
+
+function divMaker() {
+
+  for( var i = 0; i < 8000; i++){
+    var div = document.createElement('div');
+    div.classList.add('square');
+    div.addEventListener('mouseover', function(){
+    this.style.backgroundColor = brush.style.backgroundColor;
+    });
+    document.body.appendChild(div);
+  }
+}
+divMaker();
+
+
+
