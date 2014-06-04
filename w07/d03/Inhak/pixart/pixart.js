92b59d68f8d7e6773e7238ab2af8b473d74e25cb
Inhak Kim <inhakki@gmail.com>
2014-06-04 16:58:09 -0400

    pixart, commit 3 added 20 squares. commit 2 was done iwth commit 1

--- w07/d03/Inhak/pixart/pixart.js
+++ w07/d03/Inhak/pixart/pixart.js
@@ -1,11 +1,21 @@
 function colorize(){
   var form = document.getElementById('paint');
-  var square = document.querySelector('.brush');
+  var brush = document.querySelector('.brush');
   form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log('hello!');
     var color = document.getElementById('color-field').value;
-    square.style.backgroundColor = color;
+    brush.style.backgroundColor = color;
   });
 }
 colorize();
+
+function addTwentySquares(){
+  var body = document.body;
+  for (i = 0; i < 20; i++){
+    var divs = document.createElement('div');
+    divs.classList.add('square');
+    body.appendChild(divs);
+  }
+}
+addTwentySquares();
