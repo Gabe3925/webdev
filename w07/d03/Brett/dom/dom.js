a1782a5aa7e65d71ac42766c318c348cb8d5c2d2
BYWallace <bwallace90@gmail.com>
2014-06-04 16:20:44 -0400

    Added JS listeners to x-men

--- w07/d03/Brett/dom/dom.js
+++ w07/d03/Brett/dom/dom.js
@@ -81,3 +81,32 @@ function removeStryker() {
   stryker.parentNode.removeChild(stryker);
 }
 removeStryker();
+
+//////////
+///Events
+/////////
+
+function clicker() {
+  var button = document.getElementById("button");
+  button.addEventListener("click", function() {
+    alert("click!");
+  });
+}
+clicker();
+
+function clickProfessorX() {
+  var profX = document.getElementById("x");
+  profX.addEventListener("click", addGambit);
+}
+clickProfessorX();
+
+function colorize() {
+  var form = document.getElementById("paint");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(event);
+    var color = document.getElementById("input").value;
+    document.body.style.backgroundColor = color;
+  });
+}
+colorize();
