1b12e10a427c67168ec8a86e158b2f2f20c178c6
alexi215 <alexi83@gmail.com>
2014-06-04 17:38:04 -0400

    end of day

--- w07/d03/Alexi/pixart/pixart.js
+++ w07/d03/Alexi/pixart/pixart.js
@@ -7,5 +7,14 @@ var brush = document.querySelector('.brush');
 form.addEventListener('submit', function(event){
   event.preventDefault();
   brush.style.backgroundColor = input.value;
-    
 });
+
+for (var i = 0; i < 20; i++){
+  var div = document.createElement('div');
+  div.classList.add('square');
+  div.addEventListener('click', function(){
+    this.style.backgroundColor = 'green';
+  });
+
+  document.body.appendChild(div);
+}
