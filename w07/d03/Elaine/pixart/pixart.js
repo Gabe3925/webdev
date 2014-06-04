0eb005ab95ed61cc02308c7f437acf996e8b53a0
ef718 <elfang16@gmail.com>
2014-06-04 17:28:03 -0400

    Pixart

--- w07/d03/Elaine/pixart/pixart.js
+++ w07/d03/Elaine/pixart/pixart.js
@@ -8,3 +8,36 @@ function colorize() {
   });
 }
 colorize();
+
+function makeSquares() {
+  for (var i = 0; i < 8000; i++) {
+    square = document.createElement('div');
+    square.classList.add('square');
+    document.body.appendChild(square);
+  }
+}
+makeSquares();
+
+function colorSquares() {
+  var allSquares = document.querySelectorAll('.square');
+  for (var i = 0; i < 8000; i++) {
+    var eachSquare = allSquares[i];
+    eachSquare.addEventListener('mouseover', function(){
+      var color = document.querySelector('.brush').style.backgroundColor || 'navy';
+      this.style.backgroundColor = color;
+    });
+  };
+}
+colorSquares();
+
+function modifySquare() {
+  var allSquares = document.querySelectorAll('.square');
+  for (var i = 0; i < 8000; i++) {
+    var eachSquare = allSquares[i];
+    eachSquare.style.height = '10px';
+    eachSquare.style.width = '10px';
+    eachSquare.style.margin = '0px';
+  };
+
+}
+modifySquare();
