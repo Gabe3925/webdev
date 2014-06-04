90f663025ce458e9c2594258c44f68f52564e262
stephenstanwood <stephen@stanwoodsolutions.com>
2014-06-04 10:08:21 -0400

    ROMAN 1.0 (completed morning exercise)

--- w07/d03/Stephen/roman_numerals/src/Roman.js
+++ w07/d03/Stephen/roman_numerals/src/Roman.js
@@ -1,1 +1,40 @@
+/*
+* @Author: stephenstanwood
+* @Date:   2014-06-04 09:30:43
+* @Last Modified by:   stephenstanwood
+* @Last Modified time: 2014-06-04 10:06:08
+*/
 
+var ROMAN_NUMERALS = [
+    [ 'M', 1000 ],
+    [ 'CM', 900 ],
+    [ 'D',  500 ],
+    [ 'CD', 400 ],
+    [ 'C',  100 ],
+    [ 'XC',  90 ],
+    [ 'L',   50 ],
+    [ 'XL',  40 ],
+    [ 'X',   10 ],
+    [ 'IX',   9 ],
+    [ 'V',    5 ],
+    [ 'IV',   4 ],
+    [ 'I',    1 ] ];
+
+// converts a number from normal form to Roman Numerals
+function romanize( num ) {
+  if ( num < 0 ) throw "Can't convert negative number!";
+
+  var res = "";
+
+  for ( var i = 0, len = ROMAN_NUMERALS.length; i < len; i++ ) {
+    var num_sym = parseInt( num / ROMAN_NUMERALS[i][1] );
+
+    for ( var s = 0; s < num_sym; s++ ) {
+      res += ROMAN_NUMERALS[i][0];
+    }
+
+    num %= ROMAN_NUMERALS[i][1];
+  }
+
+  return res;
+}
