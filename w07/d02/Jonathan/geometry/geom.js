b52ccc0b660a6904d1c628ed2ec063f5cca82a8e
GS888 <gsnydermail@gmail.com>
2014-06-05 09:03:27 -0400

    Adding big merge.

--- w07/d02/Jonathan/geometry/geom.js
+++ w07/d02/Jonathan/geometry/geom.js
@@ -3,12 +3,50 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype = {
+
+  isSquare: function() {
+    return this.width === this.length;
+  },
+
+  area: function() {
+    return this.width * this.length;
+  },
+
+  perimeter: function() {
+    return this.width + this.length * 2;
+  }
+};
+
+var rect1 = new Rectangle(100, 100);
+rect1.isSquare();
+rect1.area();
+rect1.perimeter();
+
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
   this.sideC = sideC;
 }
+
+Triangle.prototype = {
+
+  isEquilateral: function() {
+    return this.sideA === this.sideB && this.sideB === this.sideC;
+  },
+
+  if (sideA === sideB) {
+    sideA = true;
+  }
+
+  isIsosceles: function() {
+
+  }
+
+};
+
+var tri1 = new Triangle(50, 100, 100);
 
 
 function LineSegment(x1, y1, x2, y2) {
