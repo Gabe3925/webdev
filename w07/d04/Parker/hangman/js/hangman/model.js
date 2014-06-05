24cf3bcf4f754f8562079ba200d051c94a64fc7b
parkerhart <parkerhart@gmail.com>
2014-06-05 12:01:30 -0400

    PLH

--- w07/d04/Parker/hangman/js/hangman/model.js
+++ w07/d04/Parker/hangman/js/hangman/model.js
@@ -4,8 +4,22 @@ function Hangman() {
 
 Hangman.prototype = {
   words: ['ruby', 'rails', 'javascript', 'array', 'hash', 'sinatra', 'model', 'controller', 'view', 'authentication', 'capybara', 'jasmine', 'sublime', 'terminal', 'system', 'backbone', 'function', 'prototype', 'documentation', 'development', 'data', 'closure', 'inheritance', 'scope', 'github', 'agile', 'route', 'context', 'deployment', 'database'],
+  word: '';
+  wordDisplay: '',
+  chances: 0,
+  guesses: [],
+  active: true,
+  victory: false() 
+
 
   reset: function() {
     
   }
 };
+  
+  selectRandomWord: function() 
+   	var index = Math.round((this.words.length-1) * Math.random());
+   		return this.words[index];
+     }
+}; 
+
--- w07/d04/Parker/hangman/test/spec/game_spec.js
+++ w07/d04/Parker/hangman/test/spec/game_spec.js
@@ -5,4 +5,19 @@ describe("Hangman", function() {
     game = new Hangman();
   });
 
+  describe("selectRandomWord", function() {
+
+  	it("should return the first word when random is 0.", function() {
+  		var stub = sinon.stub(Math, 'random').returns(0);
+  		expect(game.selectRandomWord()).toBe(game.words[0]);
+  		stub.restore();
+  	});
+
+  	it("should return the last word when random is 1.");
+  		var stub = sinon.stub(Math, 'random').returns(1);
+  		expect(game.selectRandomWord()).toBe(game.words.length-1]);
+  		stub.restore();
+  });
+
+});
 });
