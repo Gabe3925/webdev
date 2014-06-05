19166e32b6bccd7f4dd66c42e7dbe0ecc04f815c
alexi215 <alexi83@gmail.com>
2014-06-05 11:26:40 -0400

    hangman progressing

--- w07/d04/Alexi/hangman/test/spec/game_spec.js
+++ w07/d04/Alexi/hangman/test/spec/game_spec.js
@@ -5,4 +5,18 @@ describe("Hangman", function() {
     game = new Hangman();
   });
 
+  describe("selectRandomWord", function() {
+    it("should return the first word when random is 0", function(){
+      var stub = sinon.stub(Math, 'random').returns(0);
+      expect(game.selectRandomWord()).toBe(game.words[0]);
+      stub.restore();
+    });
+
+    it("will return the last word when random is 1.", function() {
+      var stub = sinon.stub(Math, 'random').returns(1);
+      expect(game.selectRandomWord()).toBe(game.words[game.words.length - 1]);
+      stub.restore();
+    });
+  });
 });
+
