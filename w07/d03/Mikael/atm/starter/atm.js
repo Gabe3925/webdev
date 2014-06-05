7294af9d50df241b60aad05d3e5c5ecb06a03bf8
OKitsMikael <mgd4@njit.edu>
2014-06-05 08:07:59 -0400

    withdrawals and deposits working

--- w07/d03/Mikael/atm/starter/atm.js
+++ w07/d03/Mikael/atm/starter/atm.js
@@ -1,6 +1,6 @@
 var checkbal = document.getElementById('checking-balance');
 
-var savbal = document.getElementById('savings-balance').textContent;
+var savbal = document.getElementById('savings-balance');
 
 var checkamt = document.getElementById('checking-amount');
 var savamt = document.getElementById('savings-amount');
@@ -9,8 +9,34 @@ var checkdepositbutton = document.querySelector('#checking-deposit');
 
 var savdepositbutton = document.querySelector('#savings-deposit');
 
+var checkwithdrawbutton = document.querySelector('#checking-withdraw');
+
+var savwithdrawbutton = document.querySelector('#savings-withdraw');
+
 checkdepositbutton.addEventListener('click', function() {
   var balance = parseInt(checkbal.textContent.split('$')[1])
   balance += parseInt(checkamt.value);
   checkbal.textContent = '$' + balance.toString();
+});
+
+savdepositbutton.addEventListener('click', function() {
+  var balance = parseInt(savbal.textContent.split('$')[1])
+  balance += parseInt(savamt.value);
+  savbal.textContent = '$' + balance.toString();
+});
+
+checkwithdrawbutton.addEventListener('click', function() {
+  var balance = parseInt(checkbal.textContent.split('$')[1])
+  balance -= parseInt(checkamt.value);
+  if (balance > 0) {
+    checkbal.textContent = '$' + balance.toString();
+  }
+});
+
+savwithdrawbutton.addEventListener('click', function() {
+  var balance = parseInt(savbal.textContent.split('$')[1])
+  balance -= parseInt(savamt.value);
+  if (balance > 0) {
+    savbal.textContent = '$' + balance.toString();
+  }
 });
