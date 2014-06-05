bc5422116b63740396e26f9091ac8ecc1fa7a15a
OKitsMikael <mgd4@njit.edu>
2014-06-05 08:19:11 -0400

    overdraft protection works one way

--- w07/d03/Mikael/atm/starter/atm.js
+++ w07/d03/Mikael/atm/starter/atm.js
@@ -14,27 +14,36 @@ var checkwithdrawbutton = document.querySelector('#checking-withdraw');
 var savwithdrawbutton = document.querySelector('#savings-withdraw');
 
 checkdepositbutton.addEventListener('click', function() {
-  var balance = parseInt(checkbal.textContent.split('$')[1])
+  var balance = parseInt(checkbal.textContent.split('$')[1]);
   balance += parseInt(checkamt.value);
   checkbal.textContent = '$' + balance.toString();
 });
 
 savdepositbutton.addEventListener('click', function() {
-  var balance = parseInt(savbal.textContent.split('$')[1])
+  var balance = parseInt(savbal.textContent.split('$')[1]);
   balance += parseInt(savamt.value);
   savbal.textContent = '$' + balance.toString();
 });
 
 checkwithdrawbutton.addEventListener('click', function() {
-  var balance = parseInt(checkbal.textContent.split('$')[1])
-  balance -= parseInt(checkamt.value);
-  if (balance > 0) {
+  var balance = parseInt(checkbal.textContent.split('$')[1]);
+  var totalbalance = balance + parseInt(savbal.textContent.split('$')[1]);
+  var withdraw = parseInt(checkamt.value);
+
+  if (withdraw < balance) {
+    balance -= withdraw;
     checkbal.textContent = '$' + balance.toString();
+  } else if (withdraw < totalbalance) {
+    withdraw -= balance;
+    totalbalance -= balance + withdraw;
+    checkbal.textContent = '$0';
+    savbal.textContent = '$' + totalbalance.toString();
   }
+
 });
 
 savwithdrawbutton.addEventListener('click', function() {
-  var balance = parseInt(savbal.textContent.split('$')[1])
+  var balance = parseInt(savbal.textContent.split('$')[1]);
   balance -= parseInt(savamt.value);
   if (balance > 0) {
     savbal.textContent = '$' + balance.toString();
