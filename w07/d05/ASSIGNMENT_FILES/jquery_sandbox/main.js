40bb396167af4cedba5a693da31004a96a590fff
vanderhoop <vanderhoop@me.com>
2014-06-05 15:56:59 -0400

    added comments to main

--- w07/d05/ASSIGNMENT_FILES/jquery_sandbox/main.js
+++ w07/d05/ASSIGNMENT_FILES/jquery_sandbox/main.js
@@ -2,5 +2,33 @@ console.log("================================");
 console.log("Welcome to Your Sandbox Console.");
 console.log("================================");
 
+console.log("Click on http://jquery.com to head to the docs");
+
+// Q: How do you make a DOM element a jQuery object?
+// A: Target it with a css selector and then just wrap it in cash, homie.
+      // Example:  $("#title")
+
+// Q: How do you create a NEW jQuery object you can append to the DOM?
+// A: Provide the opening tag of the type of element you want to create.
+      // Example: var newDiv = $("<div>");
+
+// IMPORTANT
+// You can't use jQuery methods on objects you haven't wrapped in cash.
+
+// Those are the basics, now research the following jQuery methods and play around with them
+// .text()
+// .html()
+// .attr()
+// .addClass()
+// .removeClass()
+// .toggleClass()
+// .css()
+// .show()
+// .hide()
+// .fadeIn()
+// .fadeOut()
+
+
+
 
 
