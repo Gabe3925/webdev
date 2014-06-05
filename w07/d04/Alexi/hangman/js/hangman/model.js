d6c039784cf851f03749fe040e10883b060aba96
alexi215 <alexi83@gmail.com>
2014-06-05 19:24:31 -0400

    end of class

--- w07/d04/Alexi/hangman/js/hangman/model.js
+++ w07/d04/Alexi/hangman/js/hangman/model.js
@@ -11,7 +11,7 @@ Hangman.prototype = {
   active: true,
   victory: false,
 
-  // Select a new random word, 
+  // Select a new random word, and reset game state.
   reset: function() {
     this.word = this.selectRandomWord();
     this.wordDisplay = '';
@@ -21,55 +21,70 @@ Hangman.prototype = {
     this.update();
   },
 
+  // Selects a random word
   selectRandomWord: function() {
-    var index = Math.round((this.words.length - 1) * Math.random()); 
+    var index = Math.round((this.words.length-1) * Math.random());
     return this.words[index];
   },
 
-  // Check to see if this letter exists within the 'guesses' array:
+  // Check to see if this letter exists within the guesses array:
   hasGuess: function(letter) {
     return this.guesses.indexOf(letter) >= 0;
   },
 
+  // Checks to see if the specified letter exists within the current word:
   hasWordLetter: function(letter) {
     return this.word.indexOf(letter) >= 0;
   },
 
+  // 
   guess: function(letter) {
     if (!this.active) return;
 
     if (letter.length > 1) {
-      throw "this is invalid input,";
+      throw "this is invalid input: it requires only one character.";
     }
 
-    if(!this.hasGuess(letter)) {
-    this.guesses.push(letter);
-      
+    if (!this.hasGuess(letter)) {
+      this.guesses.push(letter);
+
       if (!this.hasWordLetter(letter)) {
-        this.chances--;
+        this.chances -= 1;
       }
-     this.update(); 
+
+      this.update();
     }
   },
-  
-  update: function() {
-    var display = '';
 
-    for (var i = 0; i < this.word.length; i++) {
-      // do we have this letter?    
+  update: function() {
+
+    var display = '';
+    var lettersMissing = 0;
+
+    for (var i=0; i < this.word.length; i++) {
+      // do we have this letter?
       // if so, display it.
-      // otherwise, fill it with an "_"
+      // otherwise, fill it in with an "_"
+
       var letter = this.word[i];
       var hasGuess = this.hasGuess(letter);
       display += hasGuess ? letter : '_';
 
       if (!hasGuess) {
-        letterMissing += 1;
+        lettersMissing += 1;
       }
     }
-  this.victory = (!lettersMissing === 0);
-  this.active = (!lettersMissing === 0);
-  // If this game is active...
-  this.wordDisplay = this.active ? display : this.word;
+
+    this.victory = !lettersMissing;
+
+    if (this.active && this.chances) {
+      this.active = !!lettersMissing;
+    } else {
+      this.active = false;
+    }
+
+    // is the game active? if so, display the hidden word. otherwise, show the full word.
+    this.wordDisplay = this.active ? display : this.word;
   }
+
 };
