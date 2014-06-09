1ef3a2871787439943900e174cb98b71f410efc3
stephenstanwood <stephen@stanwoodsolutions.com>
2014-06-09 15:37:37 -0400

    SPOTIFY 1.0

--- w08/d01/Stephen/spotify.me/js/spotify.js
+++ w08/d01/Stephen/spotify.me/js/spotify.js
@@ -1,11 +1,42 @@
-// API Docs at: 
-// https://developer.spotify.com/technologies/web-api/search/
+/*
+* @Author: stephenstanwood
+* @Date:   2014-06-09 14:47:11
+* @Last Modified by:   stephenstanwood
+* @Last Modified time: 2014-06-09 15:35:39
+*/
 
-// Album:
-// 'http://ws.spotify.com/search/1/album.json?q=';
+var urls = {};
+urls['album'] = 'http://ws.spotify.com/search/1/album.json?q=';
+urls['artist'] = 'http://ws.spotify.com/search/1/artist.json?q=';
+urls['track'] = 'http://ws.spotify.com/search/1/track.json?q=';
 
-// Artist:
-// 'http://ws.spotify.com/search/1/artist.json?q=';
+function search() {
+  // clear previous search resultsx
+  $('body li').remove();
 
-// Track:
-// 'http://ws.spotify.com/search/1/track.json?q=';
+  var searchType = $( '#search-type option:selected' ).val();
+  var apiPath = urls[ searchType ];
+  apiPath += escape( $( '#search-keyword' ).val() );
+
+  $.getJSON( apiPath ).then( function( results ) {
+    var html = '<ul>';
+    var num = numResults( results, searchType );
+    for ( var i = 0; i < num; i++ ) {
+      var name = results[searchType + 's'][ i ][ 'name' ];
+      html += '<li>' + name + '</li>';
+    }
+
+    $( 'body' ).append( html + '</ul>' );
+  });
+}
+
+$( '#search' ).on( 'submit', function( e ) {
+  e.preventDefault();
+  search();
+});
+
+$( '#search-type' ).change( search );
+
+function numResults( results, searchType ) {
+  return results[ searchType + 's' ].length;
+}
