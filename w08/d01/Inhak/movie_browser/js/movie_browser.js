7205f1ba240e70180d50bf549cc6074f73264edf
Inhak Kim <inhakki@gmail.com>
2014-06-10 00:41:05 -0400

    movie browser half done

--- w08/d01/Inhak/movie_browser/js/movie_browser.js
+++ w08/d01/Inhak/movie_browser/js/movie_browser.js
@@ -1,2 +1,29 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+function searchMovieSelector(keyword){
+  var url = 'http://www.omdbapi.com/?s=' + escape(keyword)
+
+  $.getJSON(url).then(function(data){
+    var html = '';
+
+    for(var i=0; i < data.Search.length; i++){
+      var movie = data.Search[i];
+      html += '<option value ="' + i + '">' + movie.Title + '</option>';
+    }
+
+    $('#movie-select').html(html)
+  });
+}
+
+
+var $movieSelect = $('#movie-select');
+
+$movieSelect.hide();
+
+$('#search').on('submit', function(evt){
+  evt.preventDefault();
+  var keyword = $('#movie-search').val();
+  $movieSelect.show();
+  searchMovieSelector(keyword);
+});
