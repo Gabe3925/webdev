52799cc00a28034bc93827ee34c47030b3a28926
yihsiaol <yihsiaol@me.com>
2014-06-10 11:36:15 -0400

    add character model

--- w08/d02/Yi-Hsiao/got_hitlist_app/db/seeds.rb
+++ w08/d02/Yi-Hsiao/got_hitlist_app/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+p "Destroyed all characters"
+Character.destroy_all
+
+# Arya's hit list
+p "Adding Arya's hit list"
+Character.create(name: "King Joffrey", dead: true)
+Character.create(name: "Ser Amory", dead: true)
+Character.create(name: "The Tickler", dead: true)
+Character.create(name: "Polliver", dead: true)
+Character.create(name: "Ser Gregor", dead: true)
+Character.create(name: "Raff the Sweetling", dead: true)
+Character.create(name: "Chiswyck", dead: true)
+Character.create(name: "Weese", dead: true)
+Character.create(name: "Ilyn Payne")
+Character.create(name: "Meryn Trant")
+Character.create(name: "Queen Cersei")
+Character.create(name: "Dunsen")
+Character.create(name: "The Hound")
+
+p "Finished seeding"
