f2192af1ab472a2ff049fcab078895c26de8f0ae
alexi215 <alexi83@gmail.com>
2014-06-10 00:39:53 -0400

    movie browser homework

--- w08/d01/Alexi/movie_browser/js/movie_browser.js
+++ w08/d01/Alexi/movie_browser/js/movie_browser.js
@@ -1,2 +1,49 @@
 // API Docs at: 
 // http://www.omdbapi.com
+//  http://www.omdbapi.com/?s=True%Grit
+var keyword = '';
+
+function search(keyword) {
+  //var searchType = $("#search-type").val();
+  apiPath = 'http://www.omdbapi.com/?s=' + escape(keyword);
+  
+  $.getJSON(apiPath).then(function(results) {
+    var titles = '';
+    console.log(results);
+    for (var i=0; i < results.Search.length; i++) {
+      titles += '<option>' + results.Search[i].Title + '</option>';
+    } 
+    $("#movie-select").append(titles);
+    // $("#option-value").html("Hello");
+        //--> couldn't get it to work
+  });
+}
+
+function display(keyword) {
+  //var searchType = $("#search-type").val();
+  apiPath = 'http://www.omdbapi.com/?t=' + escape(keyword);
+  
+  $.getJSON(apiPath).then(function(results) {
+    console.log(results);
+    var poster = '<img src=' + results.Poster + '">'
+    var title = results.Title;
+    $("#movie-detail").append(title).css({ "size": "40px" });
+    // --> Poster removes title for some reason...
+    $("#movie-detail").html(poster).css({ "float": "right" });
+
+
+  });
+}
+
+$('#search').on('submit', function(evt) {
+  evt.preventDefault();
+  keyword = $('#movie-search').val();
+  search(keyword);
+});
+
+$('#movie-select').on('change', function(evt) {
+  evt.preventDefault();
+  keyword = $('#movie-select').val();
+  display(keyword); 
+})
+
