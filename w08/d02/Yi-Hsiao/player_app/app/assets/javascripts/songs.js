88694ff50141caf96d82f51492b5e3679b0289fe
yihsiaol <yihsiaol@me.com>
2014-06-10 16:59:14 -0400

    updates view when adding a song to the playlist

--- w08/d02/Yi-Hsiao/player_app/app/assets/javascripts/songs.js
+++ w08/d02/Yi-Hsiao/player_app/app/assets/javascripts/songs.js
@@ -1,15 +1,46 @@
 $(function() {
   var $form = $( "#form-add-song" );
+  var $playlist = $( "#tbody-playlist" );
 
   $form.on( "submit", function( evt ) {
     evt.preventDefault();
 
     var data = $( this ).serializeObject();
-    $.ajax({
+    addSongToDB( data )
+      .then( addSongToView )
+      .then( clearForm );
+  });
+
+  function addSongToDB( data ) {
+    return $.ajax({
       data: data,
       dataType: "json",
       type: "post",
       url: "/songs"
     });
-  });
+  }
+
+  // adds a song to the playlist view
+  function addSongToView( song ) {
+    var html = "<tr>";
+        html += "<td>" + song.title + "</td>";
+        html += "<td>" + song.artist + "</td>";
+        html += "<td>" + spotifyIframe( song.preview_url ) + "</td>";
+        html += "</tr>";
+    $playlist.append( html );
+  }
+
+  // returns html for a spotify play widget for a specified song URL
+  function spotifyIframe( spotifyUrl ) {
+    var html = '<iframe src="' + 'https://embed.spotify.com/?uri=' + spotifyUrl + '" ';
+        html += 'with="300" height="80" frameborder="0" allowtransparency="true">';
+        html += "</iframe>";
+
+    return html;
+  }
+
+  // resets the form
+  function clearForm() {
+    $form[0].reset();
+  }
 });
