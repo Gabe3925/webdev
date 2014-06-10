77ff8b1bf3701a082fd76db24ce14e44a6bb2eb6
parkerhart <parkerhart@gmail.com>
2014-06-10 17:09:52 -0400

    wonka

--- w08/d02/Parker/thrones/db/seeds.rb
+++ w08/d02/Parker/thrones/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Character.create([
+  {name: 'Eddard Stark'},
+  {name: 'Robert Baratheon'},
+  {name: 'Jaime Lannister'}
+])
