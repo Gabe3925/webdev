732d80ce25fd6e53256c027e98b618c40106a59a
stephenstanwood <stephen@stanwoodsolutions.com>
2014-06-10 08:27:55 -0400

    MOVIE BROWSER 1.0 (completed HW)

--- w08/d01/Stephen/movie_browser/js/movie_browser.js
+++ w08/d01/Stephen/movie_browser/js/movie_browser.js
@@ -2,36 +2,58 @@
 * @Author: stephenstanwood
 * @Date:   2014-06-09 16:51:16
 * @Last Modified by:   stephenstanwood
-* @Last Modified time: 2014-06-09 17:17:40
+* @Last Modified time: 2014-06-10 08:26:06
 */
 
+// hide the select button until the user searches
 $( '#movie-select' ).hide();
 
-function search( keyword ) {
-  var apiPath = 'http://www.omdbapi.com/?s=';
-  apiPath += escape( $( '#movie-search' ).val() );
-  $.getJSON( apiPath ).then( function() {
-    console.log( results );
+function search() {
+  var keyword = $( '#movie-search' ).val();
+  $.getJSON( getAPIPath( 's', keyword ) ).then( function( results ) {
+    processResults( $( '#movie-search' ).val(), results );
   });
 }
 
-function processResults( results ) {
-  console.log( results );
-  // var num = numResults( results, searchType );
-  // for ( var i = 0; i < num; i++ ) {
+function processResults( keyword, results ) {
+  $mSelect = $( '#movie-select' );
+  $mSelect.empty();
 
+  var resString = 'Movies matching *' + keyword + '*';
+  $mSelect.append( '<option>' + resString + '</option>')
 
+  // cycle through each response, adding them as options
+  var numResults = results.Search.length;
 
-  //   var name = results[searchType + 's'][ i ][ 'name' ];
-  //   html += '<li>' + name + '</li>';
+  // display the information (title/picture) for the first result
+  if ( numResults > 0 ) dispMovieInfo( results.Search[ 0 ].imdbID );
+
+  for ( var i = 0; i < numResults; i++ ) {
+    var n = results.Search[ i ].Title;
+    var id = results.Search[ i ].imdbID;
+
+    $mSelect.append( '<option value="' + id + '">' + n + '</option>' );
+  }
 }
 
-function numResults( results, searchType ) {
-  return results[ searchType + 's' ].length;
+function getAPIPath( searchType, searchOn ) {
+  var apiPath = 'http://www.omdbapi.com/?' + searchType + '=';
+  return apiPath += escape( searchOn );
+}
+
+function dispMovieInfo( id ) {
+  $.getJSON( getAPIPath( 'i', id ) ).then( function( results ) {
+    $( '#result-movie-title' ).text( results.Title );
+    $( '#result-movie-picture' ).attr( 'src', results.Poster );
+  });
 }
 
 $( '#search' ).on( 'submit', function( e ) {
   e.preventDefault();
   $( '#movie-select' ).show();
   search();
+});
+
+$( '#movie-select' ).change( function() {
+  dispMovieInfo( $( this ).val() );
 });
