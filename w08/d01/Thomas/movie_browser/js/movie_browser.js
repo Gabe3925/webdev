1caddbfd7bad6ed404a904311cab44b42919392c
thmsweaver <thmsweaver@gmail.com>
2014-06-10 12:49:06 -0400

    movie-select dropdown functioning

--- w08/d01/Thomas/movie_browser/js/movie_browser.js
+++ w08/d01/Thomas/movie_browser/js/movie_browser.js
@@ -1,2 +1,33 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+
+var keyword = ''
+$('#movie-select').hide();
+function search(keyword) {
+  var apiPath = 'http://www.omdbapi.com/?s=' + escape(keyword);
+  $.getJSON(apiPath).then(function(response) {
+
+//why does it not seem to matter if I store an initial value in titles?
+    var titles = '';
+
+    for (var i = 0; i < response.Search.length; i++) {
+      titles += ['<option>', response.Search[i].Title, '</option>'].join('');
+    }
+    $('#movie-select').append(titles);
+  });
+}
+
+
+
+$('#search').on('submit', function(evt) {
+  evt.preventDefault();
+  keyword = $('#movie-search').val();
+  $('#movie-select').show();
+  search(keyword);
+});
+
+$('#movie-select').on('change', function(evt) {
+  evt.preventDefault();
+  var userChoice = $('#movie-select').val();
+  console.log(userChoice);
+});
