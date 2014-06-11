ae623a06df6ffe741c1ad77d2767bc63c78d2fc7
yihsiaol <yihsiaol@me.com>
2014-06-10 20:54:28 -0400

    finish persist markers and showing them

--- w08/d02/Yi-Hsiao/gmaps_app/app/assets/javascripts/map.js
+++ w08/d02/Yi-Hsiao/gmaps_app/app/assets/javascripts/map.js
@@ -1,29 +1,76 @@
 $(function() {
-
-  var map;
+  var map;  // google map instance
 
   function initialize() {
+    // draw map on map canvas
+    var canvas = document.getElementById( "map-canvas" );
+    map = renderMap( canvas );
+
+    // get markers from the database and draw previously drawn markers
+    getMarkersFromDB()
+      .then( drawPreviousMarkers );
+
+    // add markers and persist any markers when a user clicks the map
+    handleMapClicks();
+  }
+
+  function renderMap( el ) {
     var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8
+      // center on DC
+      center: new google.maps.LatLng(38.904853, -77.034003),
+      zoom: 12
     };
     
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
+    var map = new google.maps.Map( el, mapOptions);
 
-    google.maps.event.addListener(map, 'click', function(event) {
-      placeMarker( event.latLng );
+    return map;
+  }
+
+  function handleMapClicks() {
+    google.maps.event.addListener( map, "click", function( evt ) {
+      var latitude = evt.latLng.k;
+      var longitude = evt.latLng.A;
+      var singleMarker = [evt.latLng];
+
+      placeMarker( evt.latLng );
+      saveMarkerToDB({ latitude: latitude, longitude: longitude });
     });
   }
 
-  function placeMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
+  // place marker on the page
+  function placeMarker( googleLatLng ) {
+    new google.maps.Marker({
+        position: googleLatLng,
         map: map
     });
+  }
 
-    // map.setCenter(location);
+  // draw old markers on the page
+  function drawPreviousMarkers( markers ) {
+    for ( var i = 0; i < markers.length; i++ ) {
+      var marker = markers[i];
+      var location = new google.maps.LatLng(marker.latitude, marker.longitude);
+      placeMarker( location );
+    }
+  }
+
+  // save markers to database
+  function saveMarkerToDB( markerAttributes ) {
+    return $.ajax({
+      data: { marker: markerAttributes },
+      type: "post",
+      url: "/markers"
+    });
+  }
+
+  // get markers from database
+  function getMarkersFromDB() {
+    return $.ajax({
+      dataType: "json",
+      type: "get",
+      url: "/markers"
+    });
   }
   
-  google.maps.event.addDomListener(window, 'load', initialize);
+  initialize();
 });
