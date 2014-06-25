01650d4b7856c06199780ff9338761d9174680d3
alexi215 <alexi83@gmail.com>
2014-06-25 09:12:30 -0400

    muppet homework complete

--- w10/d02/Alexi/muppet-upvote/js/muppets/model.js
+++ w10/d02/Alexi/muppet-upvote/js/muppets/model.js
@@ -2,15 +2,22 @@ var MuppetModel = Backbone.Model.extend({
   defaults: {
     name: '',
     image: '',
-    rating: 0,
-    upvote: (1, -1)
+    rating: 0
+  },
+
+  upvote: function(modifier) {
+    // get the current rating from the model
+    var currentRating = this.get('rating');
+    // modify the current rating with the modifier
+    // save the modified rating back into the model
+    this.save({rating: currentRating + modifier});
   }
 });
 
 var MuppetsCollection = Backbone.Collection.extend({
   model: MuppetModel,
-  localStorage: new Backbone.LocalStorage('muppet')
+  localStorage: new Backbone.LocalStorage('muppet'),
+  comparator: 'rating'
 });
 
 var muppets = new MuppetsCollection();
-muppets.comparator = 'rating';
