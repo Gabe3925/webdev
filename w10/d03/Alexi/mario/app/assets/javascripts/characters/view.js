8f579d9b907048014d94e21b6e41cfe80c5f832b
alexi215 <alexi83@gmail.com>
2014-06-25 15:17:43 -0400

    mario app completed

--- w10/d03/Alexi/mario/app/assets/javascripts/characters/view.js
+++ w10/d03/Alexi/mario/app/assets/javascripts/characters/view.js
@@ -9,18 +9,28 @@ var CharacterView = Backbone.View.extend({
   },
 
   events: {
-    'click span' : 'onRemove'
+    'click span' : 'onRemove',
+    'click img' : 'onGetCoin'
   },
 
   onRemove: function(){
-    this.model.destory();
+    this.model.destroy();
   },
 
+  onGetCoin: function(){
+    this.$el.animate({top: '-50px'}, 200).animate({top: 0}, 200);
+    document.getElementById('coin').play();
+    this.model.getCoin();
+  },
+
+  template: _.template($('#character-html').html()),
+
   render: function(){
-    var rendered = '<h2>' + this.model.get('name') + ' -- '
-      + this.model.get('coins') + '</h2>'
-      + '<span> &hearts; </span>'
-      + '<img src="' + this.model.get('img_url') + '"/>';
+    // var rendered = '<h2>' + this.model.get('name') + ' -- '
+    //   + this.model.get('coins') + '</h2>'
+    //   + '<span> &hearts; </span>'
+    //   + '<img src="' + this.model.get('img_url') + '"/>';
+    var rendered = this.template({ character: this.model });
     this.$el.html(rendered);
   }
 
@@ -34,7 +44,27 @@ var ListView = Backbone.View.extend({
   },
 
   addOne: function(character){
-    var view = new CharacterView({ model: character })
+    var view = new CharacterView({model: character});
     this.$el.append(view.el);
+  }
+});
+
+var FormView = Backbone.View.extend({
+  el: 'form',
+
+  events: {
+    'submit' : 'createCharacter'
+  },
+  
+  createCharacter: function(evt){
+    evt.preventDefault();
+    var name = this.$('[name="name"]').val();
+    var img_url = this.$('[name="img_url"]').val();
+    this.el.reset();
+
+    this.collection.create({
+      name: name,
+      img_url: img_url
+    });
   }
 });
--- w10/d03/Alexi/mario/app/assets/javascripts/main.js
+++ w10/d03/Alexi/mario/app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 $(function(){
   var characters = new CharacterCollection();
   var charactersView = new ListView({ collection: characters });
+  var formView = new FormView({ collection: characters });
   characters.fetch();
 });
