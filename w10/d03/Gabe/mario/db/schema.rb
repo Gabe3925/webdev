a148faaf6ca1dc2b4a22989f86eaa528619c3eeb
GS888 <gsnydermail@gmail.com>
2014-06-25 15:09:47 -0400

    blah

--- w10/d03/Gabe/mario/db/schema.rb
+++ w10/d03/Gabe/mario/db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625151238) do
+ActiveRecord::Schema.define(version: 20140625150705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "characters", force: true do |t|
-    t.string   "name",       null: false
+    t.string   "name",                   null: false
     t.string   "img_url"
-    t.integer  "coins"
+    t.integer  "coins",      default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- w10/d03/Gabe/mario/db/seeds.rb
+++ w10/d03/Gabe/mario/db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Character.create([
-  {name: 'Mario', img_url: 'http://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png', coins: 0 },
-  {name: 'Luigi', img_url: 'http://gamemories.files.wordpress.com/2012/08/luigi.jpg', coins: 0 },
-  ])
+  {name: 'Mario', img_url: 'http://img4.wikia.nocookie.net/__cb20120816162009/mario/images/1/15/MarioNSMB2.png'},
+  {name: 'Luigi', img_url: 'http://img1.wikia.nocookie.net/__cb20130617011001/ssb/images/6/60/Luigi_(NSMBU).png'}
+])
