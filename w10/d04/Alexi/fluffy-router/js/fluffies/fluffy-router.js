224133096f5e3b235bb808111afb0f8882f9974f
alexi215 <alexi83@gmail.com>
2014-06-26 11:42:10 -0400

    fluffy backbone project

---
+++ w10/d04/Alexi/fluffy-router/js/fluffies/fluffy-router.js
@@ -0,0 +1,44 @@
+var FluffyRouter = Backbone.Router.extend({
+
+  routes: {
+    'seal' : 'update',
+    'panda' : 'update',
+    'bunny' : 'update',
+    '*default' : 'update'
+  },
+
+  update: function(){
+    var id = Backbone.history.fragment || 'seal';
+    $('.view').hide().filter('#' + id).show();
+    this.navigate(id);
+  }
+
+  // goToSeal: function(){
+  //   console.log('seal');
+  //   $('.view').hide().filter('#seal').show();
+
+  // },
+  // goToPanda: function(){
+  //   $('.view').hide().filter('#panda').show();
+  // },
+  // goToBunny: function(){
+  //   $('.view').hide().filter('#bunny').show();
+  // }
+});
+
+var NavView = Backbone.View.extend({
+  initializer: function(options) {
+    this.listenTo(options.router, 'route', this.render);
+  },
+
+  render: function() {
+    $('a')
+      .removeClass('active')
+      .filter('[href="#' + Backbone.history.fragment +'"]')
+      .addClass('active');
+  }
+});
+
+var fluffyRouter = new FluffyRouter();
+var navView = new NavView({ router: fluffyRouter });
+Backbone.history.start();
