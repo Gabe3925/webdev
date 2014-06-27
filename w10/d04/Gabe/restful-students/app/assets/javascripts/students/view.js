076e7d3f601fcdb4420af618bb8905a42f0234af
GS888 <gsnydermail@gmail.com>
2014-06-27 10:46:44 -0400

    restful students all views show, currently messed up

--- w10/d04/Gabe/restful-students/app/assets/javascripts/students/view.js
+++ w10/d04/Gabe/restful-students/app/assets/javascripts/students/view.js
@@ -1,3 +1,4 @@
+//CREATES A STUDENT COLLECTION
 var StudentListView = Backbone.View.extend({
 
   className: 'students-list',
@@ -9,19 +10,22 @@ var StudentListView = Backbone.View.extend({
   }
 });
 
+//CREATES A STUDENT DETAIL VIEW FOR ONE STUDENT
+var StudentDetailView = Backbone.View.extend({
+  tagName: 'form',
+  className: 'student-detail',
+  template: _.template($('#student-detail').html()),
 
-
-
-// var StudentDetailView = Backbone.View.extend({
-//   tagName: 'form',
-//   className: 'student-detail',
-//   template: _.template($('#student-detail').html()),
-
-//   render: function() {
-//     var rendered = this.template(this.model.toJSON());
-//     return this.$el.html(rendered);
-//   }
-// });
+  render: function() {
+    var rendered = this.template({collection: studentsCollection });
+    return this.$el.html(rendered);
+  }
+});
 
 var studentListView = new StudentListView({collection: studentsCollection});
-studentListView.render().appendTo('body');
+//IF I RENDER THIS TO DOM, IT WILL BE SHOWING UP TWICE. ITS ALREADY GOING TO BE RENDERED INTO A DIV, WHICH THE CONSTRUCTOR MAKES BY DEFAULT.
+// studentListView.render().appendTo('body');
+
+//THIS PART CURRENTLY SHOWS ALL THE TIME-- IT SHOULD ONLY SHOW WHEN I CLICK ON A STUDENTS NAME (AND THUS GET THEIR ID) FROM THE STUDENTS COLLECTION.
+var studentDetailView = new StudentDetailView({collection: studentsCollection});
+studentDetailView.render().appendTo('body');
