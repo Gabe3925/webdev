3696ea8326e5e73b15f5d5b7ff44d9ff73ea9310
rmlatz <rmlatz@gmail.com>
2014-07-01 09:05:29 -0400

    11 1 stuff

--- w11/d01/Randy/worldcup-requirejs/public/js/countries/routers/routes.js
+++ w11/d01/Randy/worldcup-requirejs/public/js/countries/routers/routes.js
@@ -1,26 +1,28 @@
-// Requires:
-// - Backbone
-// - Countries model
-// - CountryDetailView
+define(function(require){
+  var Backbone = require('backbone');
+  var countries = require('../models/countries');
+  var CountryDetailView = require('../views/detail');
 
-var CountriesRouter = Backbone.Router.extend({
-  routes: {
-    ':id': 'country',
-    '*default': 'country'
-  },
+  var CountriesRouter = Backbone.Router.extend({
+    routes: {
+      ':id': 'country',
+      '*default': 'country'
+    },
 
-  country: function(id) {
-    if (this.view) {
-      this.view.remove();
+    country: function(id) {
+      if (this.view) {
+        this.view.remove();
+      }
+
+      if (!id) {
+        id = countries.at(0).get('alpha2Code');
+        this.navigate(id);
+      }
+
+      var country = countries.findWhere({alpha2Code: id});
+      this.view = new CountryDetailView({model: country});
+      this.view.render().appendTo('#country-main');
     }
-    
-    if (!id) {
-      id = countries.at(0).get('alpha2Code');
-      this.navigate(id);
-    }
-
-    var country = countries.findWhere({alpha2Code: id});
-    this.view = new CountryDetailView({model: country});
-    this.view.render().appendTo('#country-main');
-  }
+  });
+  return new CountriesRouter();
 });
