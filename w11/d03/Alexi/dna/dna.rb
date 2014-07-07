56b88df3ebbac15ce6d3890ab950e40428652180
alexi215 <alexi83@gmail.com>
2014-07-07 11:38:03 -0400

    morning classwork

--- w11/d03/Alexi/dna/dna.rb
+++ w11/d03/Alexi/dna/dna.rb
@@ -1,36 +1,44 @@
 require 'pry'
 
 class Dna
-attr :strand
+attr_reader :strand
+# 
 
   def initialize(strand)
-    strand = dna
+    @strand = strand
   end
 
-  def unique_bases(input)
-    array = input.split(//)
-    bases = array.uniq.length
-    return bases
+  def unique_bases
+    strand.split(//).uniq
   end
 
-
-  def num_bases(input)
-  end
-    
-  def dna()
-    a = array.count('A')
-    g = array.count('G')
-    t = array.count('T')
-    c = array.count('C')
-
-    dna_hash = Hash.new
-    dna_hash['a'] = a
-    dna_hash['g'] = g
-    dna_hash['t'] = t
-    dna_hash['c'] = c
-    
+  def num_unique_bases
+    unique_bases.length
   end
 
-  dna = "GCTCGATCGATCTAGCTAGCATGATAATCGATGATCTAGCCGGGCCCTTAGCGGATCTAGCATCGATCGACTCGATCGAGCGCTAGCTACGATCAG"
+  def num_bases(letter)
+    strand.count(letter)
+  end
 
-end
+  def base_hash
+    base_hash = {}
+    unique_bases.each do |base|
+      base_hash["#{base}"] = "#{num_bases(base)}"
+    end
+    base_hash
+  end
+
+  def compliment
+    hash = {'G' => 'C', 'A' => 'T', 'C' => 'G', 'T' => 'A'} 
+    array = strand.split(//).reverse.map do |base|
+      hash[base]
+    end
+    array.join
+  end
+end # END of Class
+
+dna = Dna.new("GTCA")
+# puts dna.unique_bases
+puts dna.num_bases('A')
+puts dna.base_hash
+puts dna.compliment
